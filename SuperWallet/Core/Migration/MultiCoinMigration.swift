a47db598518797b56c9959f47df20dbb8847cd9d
abigailshelf <abigailshelf@hotmail.com>
2021-04-14 09:15:40 +0800

    add init method

--- SuperWallet/Core/Migration/MultiCoinMigration.swift
+++ SuperWallet/Core/Migration/MultiCoinMigration.swift
@@ -6,4 +6,33 @@ import TrustKeystore
 import KeychainSwift
 
 class MultiCoinMigration {
+	
+	 struct Keys {
+        static let watchAddresses = "watchAddresses"
+    }
+
+    let keystore: Keystore
+    let appTracker: AppTracker
+    let keychain = KeychainSwift(keyPrefix: Constants.keychainKeyPrefix)
+
+    // Deprecated
+    private var watchAddresses: [String] {
+        set {
+            let data = NSKeyedArchiver.archivedData(withRootObject: newValue)
+            return UserDefaults.standard.set(data, forKey: Keys.watchAddresses)
+        }
+        get {
+            guard let data = UserDefaults.standard.data(forKey: Keys.watchAddresses) else { return [] }
+            return NSKeyedUnarchiver.unarchiveObject(with: data) as? [String] ?? []
+        }
+    }
+
+    init(
+        keystore: Keystore,
+        appTracker: AppTracker
+    ) {
+        self.keystore = keystore
+        self.appTracker = appTracker
+    }
+
 }
