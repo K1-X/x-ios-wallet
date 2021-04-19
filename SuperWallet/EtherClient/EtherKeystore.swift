c0593a30edc592debbb3ec3ba93b97879ca07dcd
abigailshelf <abigailshelf@hotmail.com>
2021-04-19 09:11:59 +0800

    init method

--- SuperWallet/EtherClient/EtherKeystore.swift
+++ SuperWallet/EtherClient/EtherKeystore.swift
@@ -26,5 +26,18 @@ class EtherKeystore: Keystore {
     let keysDirectory: URL
     let userDefaults: UserDefaults
     let storage: WalletStorage
-    
+
+    public init(
+        keychain: KeychainSwift = KeychainSwift(keyPrefix: Constants.keychainKeyPrefix),
+        keysSubfolder: String = "/keystore",
+        userDefaults: UserDefaults = UserDefaults.standard,
+        storage: WalletStorage
+    ) {
+        self.keysDirectory = URL(fileURLWithPath: datadir + keysSubfolder)
+        self.keychain = keychain
+        self.keychain.synchronizable = false
+        self.keyStore = try! KeyStore(keyDirectory: keysDirectory)
+        self.userDefaults = userDefaults
+        self.storage = storage
+    }    
 }
