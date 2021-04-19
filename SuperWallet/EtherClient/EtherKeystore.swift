f44ca81bdcefad2341d12732dde38ea8f30c6635
abigailshelf <abigailshelf@hotmail.com>
2021-04-19 09:12:30 +0800

    wallets

--- SuperWallet/EtherClient/EtherKeystore.swift
+++ SuperWallet/EtherClient/EtherKeystore.swift
@@ -48,4 +48,24 @@ class EtherKeystore: Keystore {
     var mainWallet: WalletInfo? {
         return wallets.filter { $0.mainWallet }.first
     }
+
+    var wallets: [WalletInfo] {
+        return [
+            keyStore.wallets.filter { !$0.accounts.isEmpty }.compactMap {
+                switch $0.type {
+                case .encryptedKey:
+                    let type = WalletType.privateKey($0)
+                    return WalletInfo(type: type, info: storage.get(for: type))
+                case .hierarchicalDeterministicWallet:
+                    let type = WalletType.hd($0)
+                    return WalletInfo(type: type, info: storage.get(for: type))
+                }
+            }.filter { !$0.accounts.isEmpty },
+            storage.addresses.compactMap {
+                guard let address = $0.address else { return .none }
+                let type = WalletType.address($0.coin, address)
+                return WalletInfo(type: type, info: storage.get(for: type))
+            }
+        ].flatMap { $0 }.sorted(by: { $0.info.createdAt < $1.info.createdAt })
+    }
 }
