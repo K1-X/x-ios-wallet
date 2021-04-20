ecf865997e83f8403f6122c929b1ddfafbace41d
jacobtuto <jacobtuto@hotmail.com>
2021-04-20 09:01:21 +0800

    ScanQRCodeCoordinator

--- SuperWallet/Notice/Coordinators/ScanQRCodeCoordinator.swift
+++ SuperWallet/Notice/Coordinators/ScanQRCodeCoordinator.swift
@@ -7,3 +7,17 @@ protocol ScanQRCodeCoordinatorDelegate: class {
     func didCancel(in coordinator: ScanQRCodeCoordinator)
     func didScan(result: String, in coordinator: ScanQRCodeCoordinator)
 }
+
+final class  ScanQRCodeCoordinator: NSObject, Coordinator {
+    var coordinators: [Coordinator] = []
+    weak var delegate: ScanQRCodeCoordinatorDelegate?
+
+    let navigationController: NavigationController
+    lazy var qrcodeController: QRCodeReaderViewController = {
+        let controller = QRCodeReaderViewController(cancelButtonTitle: R.string.localizable.cancel())
+        controller.delegate = self
+        controller.navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(dismiss))
+        controller.delegate = self
+        return controller
+    }()
+}
