7a8eead764bf52cdbae5879b63b42e8b2a769f73
abigailshelf <abigailshelf@hotmail.com>
2021-04-21 08:15:43 +0800

    update network coordinator

--- SuperWallet/Mine/Coordinators/AddCustomNetworkCoordinator.swift
+++ SuperWallet/Mine/Coordinators/AddCustomNetworkCoordinator.swift
@@ -8,26 +8,24 @@ protocol AddCustomNetworkCoordinatorDelegate: class {
     func didCancel(in coordinator: AddCustomNetworkCoordinator)
 }
 
-
 final class AddCustomNetworkCoordinator: Coordinator {
     let navigationController: NavigationController
     var coordinators: [Coordinator] = []
     weak var delegate: AddCustomNetworkCoordinatorDelegate?
 
-   lazy var addNetworkItem: UIBarButtonItem = {
+    lazy var addNetworkItem: UIBarButtonItem = {
         return UIBarButtonItem(
             barButtonSystemItem: .add,
             target: self,
             action: #selector(addNetwork)
         )
     }()
-    
-   lazy var addCustomNetworkController: AddCustomNetworkViewController = {
+
+    lazy var addCustomNetworkController: AddCustomNetworkViewController = {
         let controller = AddCustomNetworkViewController()
         controller.navigationItem.rightBarButtonItem = addNetworkItem
         return controller
     }()
-
 
     init(
         navigationController: NavigationController = NavigationController()
@@ -36,9 +34,7 @@ final class AddCustomNetworkCoordinator: Coordinator {
         self.navigationController.modalPresentationStyle = .formSheet
     }
 
-   
-
-start() {
+    func start() {
         navigationController.viewControllers = [addCustomNetworkController]
     }
 
@@ -52,5 +48,4 @@ start() {
             }
         }
     }
- 
 }
