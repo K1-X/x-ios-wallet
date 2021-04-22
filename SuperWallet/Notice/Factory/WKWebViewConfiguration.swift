2342d4b0758125830dd891da5e37a3867f892ccf
jacobtuto <jacobtuto@hotmail.com>
2021-04-22 09:03:05 +0800

    make

--- SuperWallet/Notice/Factory/WKWebViewConfiguration.swift
+++ SuperWallet/Notice/Factory/WKWebViewConfiguration.swift
@@ -6,5 +6,9 @@ import JavaScriptCore
 import TrustCore
 extension WKWebViewConfiguration {
 
-    
+    static func make(for server: RPCServer, address: Address, with sessionConfig: Config, in messageHandler: WKScriptMessageHandler) -> WKWebViewConfiguration {
+        let config = WKWebViewConfiguration()
+        var js = ""
+
+}    
 }
