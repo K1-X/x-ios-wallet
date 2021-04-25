1bf668b649b83c5a592df74ea41e9e7be4e8b0c5
jacobtuto <jacobtuto@hotmail.com>
2021-04-25 08:56:32 +0800

    URLViewModel

--- SuperWallet/Notice/Protocols/URLViewModel.swift
+++ SuperWallet/Notice/Protocols/URLViewModel.swift
@@ -3,3 +3,9 @@
 import Foundation
 import UIKit
 
+protocol URLViewModel {
+    var urlText: String? { get }
+    var title: String { get }
+    var imageURL: URL? { get }
+    var placeholderImage: UIImage? { get }
+}
