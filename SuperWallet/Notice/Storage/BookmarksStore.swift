86a1ab3b6afafab7b0636e407e1f9e99d783797e
jacobtuto <jacobtuto@hotmail.com>
2021-04-25 08:58:08 +0800

    let realm

--- SuperWallet/Notice/Storage/BookmarksStore.swift
+++ SuperWallet/Notice/Storage/BookmarksStore.swift
@@ -9,5 +9,11 @@ var bookmarks: Results<Bookmark> {
         return realm.objects(Bookmark.self)
             .sorted(byKeyPath: "createdAt", ascending: false)
     }
-  
+
+    let realm: Realm
+    init(
+        realm: Realm
+    ) {
+        self.realm = realm
+    }  
 }
