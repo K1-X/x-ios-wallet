762a00399e4e8fb77e7f840996036fb98920340b
jacobtuto <jacobtuto@hotmail.com>
2021-04-27 09:17:53 +0800

    init method

--- SuperWallet/Notice/Types/BrowserURLParser.swift
+++ SuperWallet/Notice/Types/BrowserURLParser.swift
@@ -3,5 +3,14 @@
 import Foundation
 
 final class BrowserURLParser {
+	let urlRegEx = try! NSRegularExpression(pattern: "^(http(s)?://)?[a-z0-9-_]+(\\.[a-z0-9-_]+)+(/)?", options: .caseInsensitive)
+    let validSchemes = ["http", "https"]
+    let engine: SearchEngine
+
+    init(
+        engine: SearchEngine = .default
+    ) {
+        self.engine = engine
+    }
     
 }
