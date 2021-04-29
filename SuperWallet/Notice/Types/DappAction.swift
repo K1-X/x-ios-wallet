97238d4f5edee9031392e07e285861568564b87c
jacobtuto <jacobtuto@hotmail.com>
2021-04-29 08:49:50 +0800

    fromCommand

--- SuperWallet/Notice/Types/DappAction.swift
+++ SuperWallet/Notice/Types/DappAction.swift
@@ -13,3 +13,26 @@ enum DappAction {
     case sendTransaction(UnconfirmedTransaction)
     case unknown
 }
+
+extension DappAction {
+    static func fromCommand(_ command: DappCommand, transfer: Transfer) -> DappAction {
+        switch command.name {
+        case .signTransaction:
+            return .signTransaction(DappAction.makeUnconfirmedTransaction(command.object, transfer: transfer))
+        case .sendTransaction:
+            return .sendTransaction(DappAction.makeUnconfirmedTransaction(command.object, transfer: transfer))
+        case .signMessage:
+            let data = command.object["data"]?.value ?? ""
+            return .signMessage(data)
+        case .signPersonalMessage:
+            let data = command.object["data"]?.value ?? ""
+            return .signPersonalMessage(data)
+        case .signTypedMessage:
+            let array = command.object["data"]?.array ?? []
+            return .signTypedMessage(array)
+        case .unknown:
+            return .unknown
+        }
+    }
+
+}
