4376267921f694c49d7ca5fcd6b024733536f583
jacobtuto <jacobtuto@hotmail.com>
2021-05-07 08:45:18 +0800

    DappCallbackValue

--- SuperWallet/Notice/Types/DappCommand.swift
+++ SuperWallet/Notice/Types/DappCommand.swift
@@ -12,3 +12,26 @@ struct DappCallback {
     let id: Int
     let value: DappCallbackValue
 }
+
+enum DappCallbackValue {
+    case signTransaction(Data)
+    case sentTransaction(Data)
+    case signMessage(Data)
+    case signPersonalMessage(Data)
+    case signTypedMessage(Data)
+
+    var object: String {
+        switch self {
+        case .signTransaction(let data):
+            return data.hexEncoded
+        case .sentTransaction(let data):
+            return data.hexEncoded
+        case .signMessage(let data):
+            return data.hexEncoded
+        case .signPersonalMessage(let data):
+            return data.hexEncoded
+        case .signTypedMessage(let data):
+            return data.hexEncoded
+        }
+    }
+}
