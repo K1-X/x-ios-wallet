bf6e3c46474b5833b390808bdda97213cac5dce0
abigailshelf <abigailshelf@hotmail.com>
2021-05-08 08:42:53 +0800

    register

--- SuperWallet/Mine/Coordinators/PushNotificationsRegistrar.swift
+++ SuperWallet/Mine/Coordinators/PushNotificationsRegistrar.swift
@@ -11,4 +11,15 @@ class PushNotificationsRegistrar: NSObject {
     private let provider = SuperWalletProviderFactory.makeProvider()
     let config = Config()
 
+    func register(launchOptions: [UIApplicationLaunchOptionsKey: Any]?) {
+        let entiity = JPUSHRegisterEntity()
+        entiity.types = Int(UNAuthorizationOptions.alert.rawValue |
+            UNAuthorizationOptions.badge.rawValue |
+            UNAuthorizationOptions.sound.rawValue)
+        JPUSHService.register(forRemoteNotificationConfig: entiity, delegate: self)
+        JPUSHService.setup(withOption: launchOptions,
+                           appKey: JPushAppKey,
+                           channel: "appstore",
+                           apsForProduction: false)
+    }
 }
