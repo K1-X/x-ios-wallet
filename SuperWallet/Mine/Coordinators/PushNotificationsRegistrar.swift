20c3bf9e32ed1c621c434118251c764b384bc9fa
abigailshelf <abigailshelf@hotmail.com>
2021-05-08 08:42:23 +0800

    push notification registrar

---
+++ SuperWallet/Mine/Coordinators/PushNotificationsRegistrar.swift
@@ -0,0 +1,11 @@
+// Copyright DApps Platform Inc. All rights reserved.
+
+import Foundation
+import UserNotifications
+import UIKit
+import Moya
+import TrustCore
+
+class PushNotificationsRegistrar: NSObject {
+
+}
