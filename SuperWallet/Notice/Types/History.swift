6114605145c58fff90ea5edfac852dd0cd84dec2
jacobtuto <jacobtuto@hotmail.com>
2021-05-09 08:39:27 +0800

    define History

--- SuperWallet/Notice/Types/History.swift
+++ SuperWallet/Notice/Types/History.swift
@@ -3,4 +3,9 @@
 import Foundation
 import RealmSwift
 
-
+final class History: Object {
+    @objc dynamic var url: String = ""
+    @objc dynamic var title: String = ""
+    @objc dynamic var createdAt: Date = Date()
+    @objc dynamic var id: String = ""
+}
