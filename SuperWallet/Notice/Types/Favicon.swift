fc653bc55fdcfb583fb250020c3b4a03092f2480
jacobtuto <jacobtuto@hotmail.com>
2021-05-09 08:38:31 +0800

    Favicon and get

--- SuperWallet/Notice/Types/Favicon.swift
+++ SuperWallet/Notice/Types/Favicon.swift
@@ -2,4 +2,11 @@
 
 import Foundation
 
-
+struct Favicon {
+    static func get(for url: URL?) -> URL? {
+        guard let host = url?.host else {
+            return .none
+        }
+        return URL(string: "https://api.statvoo.com/favicon/?url=\(host)")
+    }
+}
