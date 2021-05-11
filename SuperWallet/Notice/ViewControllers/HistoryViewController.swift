0523ea4f95aaeaaa9571cbbb47e3e1226bb9cbbf
jacobtuto <jacobtuto@hotmail.com>
2021-05-11 09:22:45 +0800

    final class HistoryViewController

--- SuperWallet/Notice/ViewControllers/HistoryViewController.swift
+++ SuperWallet/Notice/ViewControllers/HistoryViewController.swift
@@ -7,3 +7,11 @@ protocol HistoryViewControllerDelegate: class {
     func didSelect(history: History, in controller: HistoryViewController)
 }
 
+final class HistoryViewController: UIViewController {
+
+    let store: HistoryStore
+    let tableView = UITableView(frame: .zero, style: .plain)
+    lazy var viewModel: HistoriesViewModel = {
+        return HistoriesViewModel(store: store)
+    }()
+}
