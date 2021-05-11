b580d84e04dd06644e21271c554124a7b281776d
jacobtuto <jacobtuto@hotmail.com>
2021-05-11 09:23:03 +0800

    init method

--- SuperWallet/Notice/ViewControllers/HistoryViewController.swift
+++ SuperWallet/Notice/ViewControllers/HistoryViewController.swift
@@ -14,4 +14,28 @@ final class HistoryViewController: UIViewController {
     lazy var viewModel: HistoriesViewModel = {
         return HistoriesViewModel(store: store)
     }()
+
+    weak var delegate: HistoryViewControllerDelegate?
+
+    init(store: HistoryStore) {
+        self.store = store
+
+        super.init(nibName: nil, bundle: nil)
+
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.separatorStyle = .singleLine
+        tableView.rowHeight = 60
+        tableView.register(UINib(resource: R.nib.bookmarkViewCell), forCellReuseIdentifier: R.nib.bookmarkViewCell.name)
+        view.addSubview(tableView)
+        emptyView = EmptyView(title: NSLocalizedString("history.noHistory.label.title", value: "No history yet!", comment: ""))
+
+        NSLayoutConstraint.activate([
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.topAnchor.constraint(equalTo: view.topAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
 }
