6c259fe872fc911dbba57bd293d826273e16756f
jacobtuto <jacobtuto@hotmail.com>
2021-05-11 09:21:32 +0800

    some let

--- SuperWallet/Notice/Types/SendTransaction.swift
+++ SuperWallet/Notice/Types/SendTransaction.swift
@@ -3,5 +3,12 @@
 import Foundation
 
 struct SendTransaction: Decodable {
- 
+
+    let from: String
+    let to: String?
+    let value: String?
+    let gas: String?
+    let gasPrice: String?
+    let data: String?
+    let nonce: String? 
 }
