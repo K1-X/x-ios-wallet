e1f9fcbf37d6a3506fab6787a3eb707a9808f900
abigailshelf <abigailshelf@hotmail.com>
2021-05-14 08:56:44 +0800

    interval

--- SuperWallet/Mine/Types/AutoLock.swift
+++ SuperWallet/Mine/Types/AutoLock.swift
@@ -22,5 +22,15 @@ enum AutoLock: Int {
     var displayName: String {
         return "\(self.name)"
     }
+
+    var interval: Int {
+        switch self {
+        case .immediate: return 0
+        case .oneMinute: return 60
+        case .fiveMinutes: return 300
+        case .oneHour: return 3600
+        case .fiveHours: return 18000
+        }
+    }
 }
 
