576da2e4e69e9d3c0361959e1b94db510f1eb8c6
abigailshelf <abigailshelf@hotmail.com>
2021-05-14 08:56:19 +0800

    var name

--- SuperWallet/Mine/Types/AutoLock.swift
+++ SuperWallet/Mine/Types/AutoLock.swift
@@ -9,5 +9,15 @@ enum AutoLock: Int {
     case oneHour
     case fiveHours
 
+    var name: String {
+        switch self {
+        case .immediate: return NSLocalizedString("wallets.navigation.title.autolock.immediate", value: "Immediate", comment: "")
+        case .oneMinute: return NSLocalizedString("wallets.navigation.title.autolock.one.minute", value: "If away for 1 minute", comment: "")
+        case .fiveMinutes: return NSLocalizedString("wallets.navigation.title.autolock.five.minutes", value: "If away for 5 minutes", comment: "")
+        case .oneHour: return NSLocalizedString("wallets.navigation.title.autolock.one.hour", value: "If away for 1 hour", comment: "")
+        case .fiveHours: return NSLocalizedString("wallets.navigation.title.autolock.five.hour", value: "If away for 5 hours", comment: "")
+        }
+    }
+
 }
 
