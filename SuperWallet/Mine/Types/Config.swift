26a2e202646bf04e2cd33717c4f1d14500a24cd5
abigailshelf <abigailshelf@hotmail.com>
2021-05-20 08:15:01 +0800

    init method

--- SuperWallet/Mine/Types/Config.swift
+++ SuperWallet/Mine/Types/Config.swift
@@ -8,4 +8,12 @@ struct Config {
         static let currencyID = "currencyID"
     }
     
+    static let dbMigrationSchemaVersion: UInt64 = 5
+    static let current: Config = Config()
+    let defaults: UserDefaults
+    init(
+        defaults: UserDefaults = UserDefaults.standard
+    ) {
+        self.defaults = defaults
+    }    
 }
