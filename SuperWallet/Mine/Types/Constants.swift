b6a61f4b4c92d7dfbee2d53f5a14e4b65ca8dfa9
abigailshelf <abigailshelf@hotmail.com>
2021-05-22 06:30:01 +0800

    public static var

--- SuperWallet/Mine/Types/Constants.swift
+++ SuperWallet/Mine/Types/Constants.swift
@@ -12,4 +12,17 @@ public struct Constants {
     public static let defaultTelegramUsername = "superwallet"
     public static let facebookUsername = "superwalletapp"
 
+
+   public static var localizedTelegramUsernames = ["ru": "superwallet_ru", "vi": "superwallet_vn", "es": "superwallet_es", "zh": "superwallet_cn", "ja": "superwallet_jp", "de": "superwallet_de", "fr": "superwallet_fr"]
+
+    // support
+    public static let supportEmail = "support@superwalletapp.com"
+
+    public static let dappsBrowserURL = "https://dapps.superwalletapp.com/"
+    public static let dappsOpenSea = "https://opensea.io"
+    public static let dappsRinkebyOpenSea = "https://rinkeby.opensea.io"
+
+    public static let images = "https://superwalletapp.com/images"
+
+    public static let superWalletAPI = URL(string: "http://116.62.160.218:8080")!
 }
