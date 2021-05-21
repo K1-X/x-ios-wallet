20bfb47634aa8c77d5fc1d5724740a68ec848882
jacobtuto <jacobtuto@hotmail.com>
2021-05-21 08:53:25 +0800

    webview

--- SuperWallet/Notice/ViewControllers/BrowserController.swift
+++ SuperWallet/Notice/ViewControllers/BrowserController.swift
@@ -3,4 +3,11 @@ import WebKit
 
 class BrowserController: UIViewController {
 
-    }
+    var loadURL: String
+    lazy private var webview: WKWebView = {
+        self.webview = WKWebView.init(frame: view.bounds)
+        self.webview.uiDelegate = self
+        self.webview.navigationDelegate = self
+        return self.webview
+    }()
+}
