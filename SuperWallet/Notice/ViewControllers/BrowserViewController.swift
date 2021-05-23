d6c6f359a3e79bdf2449f2081e84df3807ccffed
jacobtuto <jacobtuto@hotmail.com>
2021-05-24 05:20:01 +0800

    webView

--- SuperWallet/Notice/ViewControllers/BrowserViewController.swift
+++ SuperWallet/Notice/ViewControllers/BrowserViewController.swift
@@ -20,3 +20,37 @@ protocol BrowserViewControllerDelegate: class {
     func runAction(action: BrowserAction)
     func didVisitURL(url: URL, title: String)
 }
+
+final class BrowserViewController: UIViewController {
+
+    private var myContext = 0
+    let account: WalletInfo
+    let sessionConfig: Config
+
+    private struct Keys {
+        static let estimatedProgress = "estimatedProgress"
+        static let developerExtrasEnabled = "developerExtrasEnabled"
+        static let URL = "URL"
+        static let ClientName = "SuperWallet"
+    }
+
+    private lazy var userClient: String = {
+        return Keys.ClientName + "/" + (Bundle.main.versionNumber ?? "")
+    }()
+
+    lazy var webView: WKWebView = {
+        let webView = WKWebView(
+            frame: .zero,
+            configuration: self.config
+        )
+        webView.allowsBackForwardNavigationGestures = true
+        webView.translatesAutoresizingMaskIntoConstraints = false
+        webView.navigationDelegate = self
+        if isDebug {
+            webView.configuration.preferences.setValue(true, forKey: Keys.developerExtrasEnabled)
+        }
+        return webView
+    }()
+
+
+}
