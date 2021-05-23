e4fe5d5849fbcd3c6751dae90f9488f90009d78d
jacobtuto <jacobtuto@hotmail.com>
2021-05-24 06:30:01 +0800

    some methods handleError

--- SuperWallet/Notice/ViewControllers/BrowserViewController.swift
+++ SuperWallet/Notice/ViewControllers/BrowserViewController.swift
@@ -79,5 +79,169 @@ final class BrowserViewController: UIViewController {
         config.websiteDataStore = WKWebsiteDataStore.default()
         return config
     }()
+    
+    let server: RPCServer
 
+    init(
+        account: WalletInfo,
+        config: Config,
+        server: RPCServer
+    ) {
+        self.account = account
+        self.sessionConfig = config
+        self.server = server
+
+        super.init(nibName: nil, bundle: nil)
+
+        view.addSubview(webView)
+        injectUserAgent()
+
+        webView.addSubview(progressView)
+        webView.bringSubview(toFront: progressView)
+        view.addSubview(errorView)
+
+        NSLayoutConstraint.activate([
+            webView.topAnchor.constraint(equalTo: topLayoutGuide.bottomAnchor),
+            webView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            webView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            webView.bottomAnchor.constraint(equalTo: bottomLayoutGuide.topAnchor),
+
+            progressView.topAnchor.constraint(equalTo: view.layoutGuide.topAnchor),
+            progressView.leadingAnchor.constraint(equalTo: webView.leadingAnchor),
+            progressView.trailingAnchor.constraint(equalTo: webView.trailingAnchor),
+            progressView.heightAnchor.constraint(equalToConstant: 2),
+
+            errorView.topAnchor.constraint(equalTo: webView.topAnchor),
+            errorView.leadingAnchor.constraint(equalTo: webView.leadingAnchor),
+            errorView.trailingAnchor.constraint(equalTo: webView.trailingAnchor),
+            errorView.bottomAnchor.constraint(equalTo: webView.bottomAnchor)
+        ])
+        view.backgroundColor = .white
+        webView.addObserver(self, forKeyPath: Keys.estimatedProgress, options: .new, context: &myContext)
+        webView.addObserver(self, forKeyPath: Keys.URL, options: [.new, .initial], context: &myContext)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
+        browserNavBar?.browserDelegate = self
+        refreshURL()
+    }
+
+    private func injectUserAgent() {
+        webView.evaluateJavaScript("navigator.userAgent") { [weak self] result, _ in
+            guard let `self` = self, let currentUserAgent = result as? String else { return }
+            self.webView.customUserAgent = currentUserAgent + " " + self.userClient
+        }
+    }
+
+    func goTo(url: URL) {
+        webView.load(URLRequest(url: url))
+    }
+
+    func notifyFinish(callbackID: Int, value: Result<DappCallback, DAppError>) {
+        let script: String = {
+            switch value {
+            case .success(let result):
+                return "executeCallback(\(callbackID), null, \"\(result.value.object)\")"
+            case .failure(let error):
+                return "executeCallback(\(callbackID), \"\(error)\", null)"
+            }
+        }()
+        webView.evaluateJavaScript(script, completionHandler: nil)
+    }
+
+    func goHome() {
+        guard let url = URL(string: Constants.dappsBrowserURL) else { return }
+        var request = URLRequest(url: url)
+        request.cachePolicy = .returnCacheDataElseLoad
+        hideErrorView()
+        webView.load(request)
+        browserNavBar?.textField.text = url.absoluteString
+    }
+
+    func reload() {
+        hideErrorView()
+        webView.reload()
+    }
+
+    private func stopLoading() {
+        webView.stopLoading()
+    }
+
+    private func refreshURL() {
+        browserNavBar?.textField.text = webView.url?.absoluteString
+        browserNavBar?.backButton.isHidden = !webView.canGoBack
+
+    }
+
+    private func recordURL() {
+        guard let url = webView.url else {
+            return
+        }
+        delegate?.didVisitURL(url: url, title: webView.title ?? "")
+    }
+
+    private func changeURL(_ url: URL) {
+        delegate?.runAction(action: .changeURL(url))
+        refreshURL()
+    }
+
+    private func hideErrorView() {
+        errorView.isHidden = true
+    }
+
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey: Any]?, context: UnsafeMutableRawPointer?) {
+        guard let change = change else { return }
+        if context != &myContext {
+            super.observeValue(forKeyPath: keyPath, of: object, change: change, context: context)
+            return
+        }
+        if keyPath == Keys.estimatedProgress {
+            if let progress = (change[NSKeyValueChangeKey.newKey] as AnyObject).floatValue {
+                progressView.progress = progress
+                progressView.isHidden = progress == 1
+            }
+        } else if keyPath == Keys.URL {
+            if let url = webView.url {
+                self.browserNavBar?.textField.text = url.absoluteString
+                changeURL(url)
+            }
+        }
+    }
+
+    deinit {
+        webView.removeObserver(self, forKeyPath: Keys.estimatedProgress)
+        webView.removeObserver(self, forKeyPath: Keys.URL)
+    }
+
+    func addBookmark() {
+        guard let url = webView.url?.absoluteString else { return }
+        guard let title = webView.title else { return }
+        delegate?.runAction(action: .addBookmark(bookmark: Bookmark(url: url, title: title)))
+    }
+
+    @objc private func showBookmarks() {
+        delegate?.runAction(action: .bookmarks)
+    }
+
+    @objc private func history() {
+        delegate?.runAction(action: .history)
+    }
+
+    func handleError(error: Error) {
+        if error.code == NSURLErrorCancelled {
+            return
+        } else {
+            if error.domain == NSURLErrorDomain,
+                let failedURL = (error as NSError).userInfo[NSURLErrorFailingURLErrorKey] as? URL {
+                changeURL(failedURL)
+            }
+            errorView.show(error: error)
+        }
+    }
 }
