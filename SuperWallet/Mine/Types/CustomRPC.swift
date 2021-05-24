766c027a8ef8a5c3b26d616a06567f9246766284
abigailshelf <abigailshelf@hotmail.com>
2021-05-25 07:50:01 +0800

    customRPC and mineAction

---
+++ SuperWallet/Mine/Types/CustomRPC.swift
@@ -0,0 +1,8 @@
+// Copyright DApps Platform Inc. All rights reserved.
+
+import Foundation
+import RealmSwift
+
+final class CustomRPC: Object {
+
+}
---
+++ SuperWallet/Mine/Types/MineAction.swift
@@ -0,0 +1,11 @@
+// Copyright DApps Platform Inc. All rights reserved.
+
+import Foundation
+
+enum MineAction {
+    case publishToken
+    case addressBook
+    case walletManager
+    case safetyManager
+    case setting
+}
