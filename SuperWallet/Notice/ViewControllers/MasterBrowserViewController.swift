2ef06edf3a72b8a962f1da901f91045e9ea82f6f
jacobtuto <jacobtuto@hotmail.com>
2021-05-26 06:30:01 +0800

    qrcodeButton

--- SuperWallet/Notice/ViewControllers/MasterBrowserViewController.swift
+++ SuperWallet/Notice/ViewControllers/MasterBrowserViewController.swift
@@ -17,3 +17,27 @@ enum BookmarksViewType: Int {
     case bookmarks
     case history
 }
+final class MasterBrowserViewController: UIViewController {
+
+    private lazy var segmentController: UISegmentedControl = {
+        let items = [
+            R.string.localizable.new(),
+            R.string.localizable.bookmarks(),
+            R.string.localizable.history()
+        ]
+        let segmentedControl = UISegmentedControl(items: items)
+        segmentedControl.addTarget(self, action: #selector(selectionDidChange(_:)), for: .valueChanged)
+        segmentedControl.tintColor = .clear
+        return segmentedControl
+    }()
+
+    private lazy var qrcodeButton: UIButton = {
+        let button = Button(size: .normal, style: .borderless)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setImage(R.image.browser_scan(), for: .normal)
+        button.addTarget(self, action: #selector(qrReader), for: .touchUpInside)
+        return button
+    }()
+
+}
+
