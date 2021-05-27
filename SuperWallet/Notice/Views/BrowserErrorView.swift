abd5762973e4723a8b7d57578965ba5dd909282e
jacobtuto <jacobtuto@hotmail.com>
2021-05-28 07:50:01 +0800

    textLabel

--- SuperWallet/Notice/Views/BrowserErrorView.swift
+++ SuperWallet/Notice/Views/BrowserErrorView.swift
@@ -5,3 +5,23 @@ import UIKit
 protocol BrowserErrorViewDelegate: class {
     func didTapReload(_ sender: Button)
 }
+
+final class BrowserErrorView: UIView {
+
+    weak var delegate: BrowserErrorViewDelegate?
+
+    private let topMargin: CGFloat = 120
+    private let leftMargin: CGFloat = 40
+    private let buttonTopMargin: CGFloat = 6
+
+    lazy var textLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 0
+        label.textColor = Colors.gray
+        label.font = UIFont.systemFont(ofSize: 18)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+
+}
+
