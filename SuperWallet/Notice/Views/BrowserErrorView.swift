c64fbbd5bf97ba7aae3f555d3c87c3b0aaef3700
jacobtuto <jacobtuto@hotmail.com>
2021-05-28 09:20:01 +0800

    init method

--- SuperWallet/Notice/Views/BrowserErrorView.swift
+++ SuperWallet/Notice/Views/BrowserErrorView.swift
@@ -31,5 +31,19 @@ final class BrowserErrorView: UIView {
         button.sizeToFit()
         return button
     }()
+
+    init() {
+        super.init(frame: CGRect.zero)
+        finishInit()
+    }
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        finishInit()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
 
