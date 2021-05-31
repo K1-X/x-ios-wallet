23a8d7ea923b152697d9131eceebb35a31291085
jacobtuto <jacobtuto@hotmail.com>
2021-06-01 05:50:01 +0800

    timeInterval

--- SuperWallet/Notice/Views/ADHeaderView.swift
+++ SuperWallet/Notice/Views/ADHeaderView.swift
@@ -2,6 +2,12 @@ import UIKit
 import Kingfisher
 
 class ADHeaderView: UIView, UIScrollViewDelegate {
-    
+    var timeInterval: TimeInterval = 4
+
+    private var imageUrls: [String] = [String]()
+    private var imageArray: [UIImageView] = [UIImageView]()
+    private var tapAction: (Int) -> Void
+    private var currentIndex: Int = 0
+    private weak var timer: Timer?    
 
 }
