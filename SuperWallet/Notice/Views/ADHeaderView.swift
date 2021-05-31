825a1de4cb5440ae166261c93633b0b6a7e371a2
jacobtuto <jacobtuto@hotmail.com>
2021-06-01 06:30:01 +0800

    scrollView

--- SuperWallet/Notice/Views/ADHeaderView.swift
+++ SuperWallet/Notice/Views/ADHeaderView.swift
@@ -10,4 +10,18 @@ class ADHeaderView: UIView, UIScrollViewDelegate {
     private var currentIndex: Int = 0
     private weak var timer: Timer?    
 
+
+    private lazy var scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.scrollsToTop = false
+        scrollView.isPagingEnabled = true
+        scrollView.bounces = false
+        scrollView.frame = self.bounds
+        scrollView.delegate = self
+        scrollView.showsHorizontalScrollIndicator = false
+        scrollView.decelerationRate = 1
+        scrollView.setContentOffset(CGPoint(x: self.frame.width, y: 0), animated: false)
+        scrollView.contentSize = CGSize(width: self.frame.size.width * 3.0, height: 0)
+        return scrollView
+    }()
 }
