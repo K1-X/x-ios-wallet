51483cd5deccbed1ea2351bd6f2c0a672e6033cb
jacobtuto <jacobtuto@hotmail.com>
2021-06-01 07:20:01 +0800

    pageControl

--- SuperWallet/Notice/Views/ADHeaderView.swift
+++ SuperWallet/Notice/Views/ADHeaderView.swift
@@ -24,4 +24,19 @@ class ADHeaderView: UIView, UIScrollViewDelegate {
         scrollView.contentSize = CGSize(width: self.frame.size.width * 3.0, height: 0)
         return scrollView
     }()
+
+    lazy var pageControl: SPPageControl = {
+        let pageControl = SPPageControl()//PageControl
+        pageControl.frame = CGRect.init(x: 0, y: scrollView.frame.maxY-20, width: self.frame.size.width, height: 20)
+        pageControl.numberOfPages = self.imageUrls.count//
+        pageControl.isSquare = true//
+        pageControl.currentWidthMultiple = 1//3
+        pageControl.currentColor = Colors.white
+        pageControl.otherColor = Colors.lightGray
+        pageControl.pointSize = CGSize.init(width: 30, height: 2)//size
+        pageControl.clickPoint { (index) in//
+            self.scrollView.setContentOffset(CGPoint.init(x: self.frame.size.width * CGFloat(index!), y: 0), animated: true)
+        }
+        return pageControl
+    }()
 }
