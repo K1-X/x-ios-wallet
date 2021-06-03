bb4d4963ffe33e3d891fab608b0bb783a94f95e2
abigailshelf <abigailshelf@hotmail.com>
2021-06-04 06:30:01 +0800

    case

--- SuperWallet/Mine/Types/ServiceProvider.swift
+++ SuperWallet/Mine/Types/ServiceProvider.swift
@@ -5,5 +5,14 @@ import UIKit
 import Rswift
 
 enum URLServiceProvider {
-    
+    case twitter
+    case telegram
+    case facebook
+    case discord
+    case helpCenter
+    case sourceCode
+    case privacyPolicy
+    case termsOfService
+    case infura
+    case dappsOpenSea    
 }
