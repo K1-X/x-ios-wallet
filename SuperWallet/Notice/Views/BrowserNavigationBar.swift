0a9d473ad2f46cdc8a8675c2e0704a3f3cdb04a6
jacobtuto <jacobtuto@hotmail.com>
2021-06-05 06:30:01 +0800

    BrowserNavigationBar

--- SuperWallet/Notice/Views/BrowserNavigationBar.swift
+++ SuperWallet/Notice/Views/BrowserNavigationBar.swift
@@ -5,3 +5,18 @@ import UIKit
 protocol BrowserNavigationBarDelegate: class {
     func did(action: BrowserNavigation)
 }
+
+final class BrowserNavigationBar: UINavigationBar {
+
+    let textField = UITextField()
+    let moreButton = UIButton()
+    let homeButton = UIButton()
+    let backButton = UIButton()
+    weak var browserDelegate: BrowserNavigationBarDelegate?
+
+    private struct Layout {
+        static let width: CGFloat = 34
+        static let moreButtonWidth: CGFloat = 24
+    }
+
+}
