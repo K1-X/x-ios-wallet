9cc473a03fc61a7ce9d603d94e5e68c045e4461a
abigailshelf <abigailshelf@hotmail.com>
2021-06-07 07:50:01 +0800

    errorDescription

--- SuperWallet/Mine/Types/SendInputErrors.swift
+++ SuperWallet/Mine/Types/SendInputErrors.swift
@@ -3,5 +3,15 @@
 import Foundation
 
 enum SendInputErrors: LocalizedError {
-   
+    case emptyClipBoard
+    case wrongInput
+
+    var errorDescription: String? {
+        switch self {
+        case .emptyClipBoard:
+            return NSLocalizedString("send.error.emptyClipBoard", value: "Empty ClipBoard", comment: "")
+        case .wrongInput:
+            return NSLocalizedString("send.error.wrongInput", value: "Wrong Input", comment: "")
+        }
+    }   
 }
