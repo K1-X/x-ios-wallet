8e6dd32147954ae2122f6d3789023b0f7ffd0109
jacobtuto <jacobtuto@hotmail.com>
2021-06-08 05:50:01 +0800

    init method

--- SuperWallet/Notice/Views/SPPageControl.swift
+++ SuperWallet/Notice/Views/SPPageControl.swift
@@ -2,5 +2,25 @@ import UIKit
 
 class SPPageControl: UIControl {
 
+    var localNumberOfPages = NSInteger()//
+    var localCurrentPage = NSInteger()//
+    var localPointSize = CGSize()//
+    var localPointSpace = CGFloat()//
+    var localOtherColor = UIColor()//
+    var localCurrentColor = UIColor()//
+    var localOtherImage: UIImage?//
+    var localCurrentImage: UIImage?//
+    var localIsSquare = Bool()//
+    var localCurrentWidthMultiple = CGFloat()//
+    var localOtherBorderColor: UIColor?//layerColor
+    var localOtherBorderWidth: CGFloat?//layer
+    var localCurrentBorderColor: UIColor?//layerColor
+    var localCurrentBorderWidth: CGFloat?//layer
+    var clickIndex: ((_ result: NSInteger?) -> Void)?
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        initialize()
+    }
     
 }
