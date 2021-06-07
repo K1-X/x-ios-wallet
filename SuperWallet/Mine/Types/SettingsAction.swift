d517af1786bcb458d01f5ae3df4b3a15694ce879
abigailshelf <abigailshelf@hotmail.com>
2021-06-07 09:00:01 +0800

    SettingAction

---
+++ SuperWallet/Mine/Types/SettingsAction.swift
@@ -0,0 +1,12 @@
+// Copyright DApps Platform Inc. All rights reserved.
+
+import Foundation
+
+enum SettingsAction {
+    case chooseChain
+    case feedback
+    case updateVersion
+    case clearCache
+    case aboutUS
+    case logout
+}
