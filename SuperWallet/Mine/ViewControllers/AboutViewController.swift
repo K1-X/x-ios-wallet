f1859680c368c33149343b228bf155e9f56cff4a
abigailshelf <abigailshelf@hotmail.com>
2021-06-09 05:50:01 +0800

    AboutViewControllerDelegate class

--- SuperWallet/Mine/ViewControllers/AboutViewController.swift
+++ SuperWallet/Mine/ViewControllers/AboutViewController.swift
@@ -3,3 +3,8 @@ import Foundation
 import UIKit
 import Eureka
 import MessageUI
+
+protocol AboutViewControllerDelegate: class {
+    func didPressURL(_ url: URL, in controller: AboutViewController)
+}
+
