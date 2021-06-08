1d1269cb9284b17ce4a57c3901441ca6898262c9
abigailshelf <abigailshelf@hotmail.com>
2021-06-09 07:20:01 +0800

    viewDidLoad

--- SuperWallet/Mine/ViewControllers/AboutViewController.swift
+++ SuperWallet/Mine/ViewControllers/AboutViewController.swift
@@ -16,5 +16,43 @@ final class AboutViewController: FormViewController {
     init() {
         super.init(nibName: nil, bundle: nil)
     }
+    override func viewDidLoad() {
+        super.viewDidLoad()
 
+        navigationItem.title = viewModel.title
+
+        form +++ Section()
+
+            <<< linkProvider(type: .sourceCode)
+
+            +++ Section()
+
+            <<< linkProvider(type: .privacyPolicy)
+            <<< linkProvider(type: .termsOfService)
+
+            +++ Section()
+
+            <<< AppFormAppearance.button { button in
+                button.title = R.string.localizable.settingsEmailUsButtonTitle()
+            }.onCellSelection { [weak self] _, _  in
+                self?.sendUsEmail()
+            }.cellUpdate({ (cell, _) in
+                    cell.accessoryType = .disclosureIndicator
+                    cell.textLabel?.textAlignment = .left
+                    cell.textLabel?.textColor = .black
+            })
+
+            +++ Section()
+
+            <<< TextRow {
+                $0.title = R.string.localizable.settingsVersionLabelTitle()
+                $0.value = Bundle.main.fullVersion
+                $0.disabled = true
+            }
+
+            +++ Section(R.string.localizable.poweredBy())
+
+            <<< linkProvider(type: .infura)
+            <<< linkProvider(type: .dappsOpenSea)
+    }
 }
