031ded9cbc607605c09de2b7337ed5eecc08386e
abigailshelf <abigailshelf@hotmail.com>
2021-06-09 06:30:01 +0800

    init method

--- SuperWallet/Mine/ViewControllers/AboutViewController.swift
+++ SuperWallet/Mine/ViewControllers/AboutViewController.swift
@@ -8,3 +8,13 @@ protocol AboutViewControllerDelegate: class {
     func didPressURL(_ url: URL, in controller: AboutViewController)
 }
 
+final class AboutViewController: FormViewController {
+
+    let viewModel = AboutViewModel()
+    weak var delegate: AboutViewControllerDelegate?
+
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+
+}
