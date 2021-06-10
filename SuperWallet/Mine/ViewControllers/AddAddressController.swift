4ee6530277ff0139978edcabafecbb38d9621382
abigailshelf <abigailshelf@hotmail.com>
2021-06-11 05:00:01 +0800

    AddAddressController.swift added

---
+++ SuperWallet/Mine/ViewControllers/AddAddressController.swift
@@ -0,0 +1,11 @@
+// Copyright DApps Platform Inc. All rights reserved.
+
+import UIKit
+import SnapKit
+import MBProgressHUD
+import TrustCore
+
+class AddAddressController: UIViewController {
+
+    
+}
