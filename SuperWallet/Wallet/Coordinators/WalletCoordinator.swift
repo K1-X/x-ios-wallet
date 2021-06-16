4d3b8b2567744786a1cff3d7d8d48bef94443d6c
jacobtuto <jacobtuto@hotmail.com>
2021-06-17 06:30:01 +0800

    WalletCoordinatorDelegate

--- SuperWallet/Wallet/Coordinators/WalletCoordinator.swift
+++ SuperWallet/Wallet/Coordinators/WalletCoordinator.swift
@@ -2,3 +2,10 @@ import Foundation
 import TrustCore
 import TrustKeystore
 import UIKit
+
+protocol WalletCoordinatorDelegate: class {
+    func didFinish(with account: WalletInfo, in coordinator: WalletCoordinator)
+    func didCancel(in coordinator: WalletCoordinator)
+}
+
+
