0655bd2d5a858dfc9edae030593b7301e45f8548
abigailshelf <abigailshelf@hotmail.com>
2021-06-18 07:50:01 +0800

    AddressBookController

--- SuperWallet/Mine/ViewControllers/AddressBookController.swift
+++ SuperWallet/Mine/ViewControllers/AddressBookController.swift
@@ -7,3 +7,29 @@ protocol AddressBookControllerDelegate: class {
     func didClickAddButton(bookStorage: AddressBookStorage, viewController: AddressBookController)
     func didClickAddress(bookStorage: AddressBookStorage, addressBook: AddressBook, viewController: AddressBookController)
 }
+
+class AddressBookController: UIViewController {
+
+    private var config = Config()
+    var viewModel: AddressBookModel
+    weak var delegate: AddressBookControllerDelegate?
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView(frame: .zero, style: .plain)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.separatorStyle = .none
+        tableView.backgroundColor = Colors.veryLightGray
+        tableView.backgroundColor = Colors.white
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.tableFooterView = UIView()
+        tableView.register(UINib(resource: R.nib.addressBookCell), forCellReuseIdentifier: R.nib.addressBookCell.name)
+        return tableView
+    }()
+
+    init(viewModel: AddressBookModel) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+
+}
+
