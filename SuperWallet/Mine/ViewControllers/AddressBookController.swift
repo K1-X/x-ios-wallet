ffc45eed2682c053b2beffd520e4ab20475efece
abigailshelf <abigailshelf@hotmail.com>
2021-06-18 09:00:01 +0800

    viewDidLoad

--- SuperWallet/Mine/ViewControllers/AddressBookController.swift
+++ SuperWallet/Mine/ViewControllers/AddressBookController.swift
@@ -30,6 +30,28 @@ class AddressBookController: UIViewController {
         self.viewModel = viewModel
         super.init(nibName: nil, bundle: nil)
     }
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = Colors.white
+        title = R.string.localizable.mineAddressBookTitle()
+
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(pushAddAddress))
+        if #available(iOS 11.0, *) {
+            tableView.contentInsetAdjustmentBehavior = .never
+        } else {
+            self.automaticallyAdjustsScrollViewInsets = false
+        }
+        view.addSubview(tableView)
+        tableView.snp.makeConstraints { (make) in
+            make.top.equalToSuperview().offset(currentNaviHeight)
+            make.leading.trailing.equalToSuperview()
+            make.bottom.equalToSuperview()
+        }
+        tableView.layoutIfNeeded()
+    }
 }
 
