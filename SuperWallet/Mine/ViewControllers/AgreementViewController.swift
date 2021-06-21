dc3826235f21c332a6fc79a65ba3a97517ab018b
abigailshelf <abigailshelf@hotmail.com>
2021-06-21 09:00:01 +0800

    viewDidLoad

--- SuperWallet/Mine/ViewControllers/AgreementViewController.swift
+++ SuperWallet/Mine/ViewControllers/AgreementViewController.swift
@@ -9,4 +9,16 @@ class AgreementViewController: UIViewController {
         return webView
     }()
 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = Colors.white
+        navigationItem.title = ""
+        view.addSubview(webView)
+        webView.frame = view.bounds
+        let path = Bundle.main.bundlePath
+        let url = URL.init(fileURLWithPath: path)
+        let htmlPath = Bundle.main.path(forResource: "agreement", ofType: "html")
+        let htmlContent = try!String.init(contentsOfFile: htmlPath!)
+        webView.loadHTMLString(htmlContent, baseURL: url)
+    }
 }
