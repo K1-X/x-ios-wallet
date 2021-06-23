96a126bf736e42c540f23e7c4c4aa25390da5c5e
abigailshelf <abigailshelf@hotmail.com>
2021-06-24 07:20:01 +0800

    NotificationsViewController.swift added

---
+++ SuperWallet/Mine/ViewControllers/NotificationsViewController.swift
@@ -0,0 +1,10 @@
+// Copyright DApps Platform Inc. All rights reserved.
+
+import UIKit
+import Eureka
+
+struct NotificationChange: Codable {
+    let isEnabled: Bool
+    let preferences: Preferences
+}
+
