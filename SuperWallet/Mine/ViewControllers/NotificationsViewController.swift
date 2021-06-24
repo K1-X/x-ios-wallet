c85ee2331b951264e32d25e27178c2672c4d4085
abigailshelf <abigailshelf@hotmail.com>
2021-06-24 09:00:01 +0800

    showOptionsCondition

--- SuperWallet/Mine/ViewControllers/NotificationsViewController.swift
+++ SuperWallet/Mine/ViewControllers/NotificationsViewController.swift
@@ -12,3 +12,27 @@ enum NotificationChanged {
     case state(isEnabled: Bool)
     case preferences(Preferences)
 }
+
+
+final class NotificationsViewController: FormViewController {
+
+    private let viewModel = NotificationsViewModel()
+    private let preferencesController: PreferencesController
+
+    private struct Keys {
+        static let pushNotifications = "pushNotifications"
+        static let payment = "payment"
+    }
+
+    var didChange: ((_ change: NotificationChanged) -> Void)?
+
+    private static var isPushNotificationEnabled: Bool {
+        guard let settings = UIApplication.shared.currentUserNotificationSettings else { return false }
+        return UIApplication.shared.isRegisteredForRemoteNotifications && !settings.types.isEmpty
+    }
+
+    private var showOptionsCondition: Condition {
+        return Condition.predicate(NSPredicate(format: "$\(Keys.pushNotifications) == false"))
+    }
+
+}
