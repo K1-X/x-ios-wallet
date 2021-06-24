73dc08c2b70eed373ce4524a482632f2690fe0f6
abigailshelf <abigailshelf@hotmail.com>
2021-06-24 10:10:01 +0800

    viewDidLoad

--- SuperWallet/Mine/ViewControllers/NotificationsViewController.swift
+++ SuperWallet/Mine/ViewControllers/NotificationsViewController.swift
@@ -41,4 +41,38 @@ final class NotificationsViewController: FormViewController {
         self.preferencesController = preferencesController
         super.init(nibName: nil, bundle: nil)
     }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        navigationItem.title = viewModel.title
+
+        form +++ Section()
+
+            <<< SwitchRow(Keys.pushNotifications) {
+                $0.title = NSLocalizedString("settings.allowPushNotifications.button.title", value: "Allow Push Notifications", comment: "")
+                $0.value = NotificationsViewController.isPushNotificationEnabled
+            }.onChange { [unowned self] row in
+                self.didChange?(.state(isEnabled: row.value ?? false))
+            }
+
+        +++ Section(
+            footer: NSLocalizedString(
+                "settings.pushNotifications.allowPushNotifications.footer",
+                value: "You will be notified for sent and received transactions.",
+                comment: ""
+            )
+        ) {
+            $0.hidden = showOptionsCondition
+        }
+
+        <<< SwitchRow(Keys.payment) { [weak self] in
+            $0.title = NSLocalizedString("settings.pushNotifications.payment.button.title", value: "Sent and Receive", comment: "")
+            $0.value = true
+            $0.hidden = self?.showOptionsCondition
+            $0.disabled = Condition(booleanLiteral: true)
+        }.cellSetup { cell, _ in
+            cell.switchControl.isEnabled = false
+        }
+    }
 }
