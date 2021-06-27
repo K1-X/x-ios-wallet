ae0c7034c345e3971d8cefd9e658e9d35c802b9e
abigailshelf <abigailshelf@hotmail.com>
2021-06-28 07:20:01 +0800

    viewDidLoad

--- SuperWallet/Mine/ViewControllers/PrivacyViewController.swift
+++ SuperWallet/Mine/ViewControllers/PrivacyViewController.swift
@@ -11,4 +11,29 @@ final class PrivacyViewController: FormViewController {
     private var amountRow: SwitchRow? {
         return form.rowBy(tag: viewModel.answer.rawValue) as? SwitchRow
     }    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        navigationItem.title = viewModel.title
+
+        form +++ Section(footer: viewModel.answer.description)
+
+        <<< SwitchRow(viewModel.answer.rawValue) {
+            $0.title = viewModel.answer.title
+            $0.value = viewModel.answer.isEnabled
+        }.onChange { [weak self] row in
+            guard let enabled = row.value else { return }
+            self?.viewModel.answer.update(with: enabled)
+        }
+        form +++ Section(footer: viewModel.branch.description)
+        <<< SwitchRow {
+            $0.title = viewModel.branch.title
+            $0.value = viewModel.branch.isEnabled
+        }.onChange { [weak self] row in
+             guard let enabled = row.value else { return }
+             self?.viewModel.branch.update(with: enabled)
+             Branch.setTrackingDisabled(!enabled)
+        }
+    }
 }
