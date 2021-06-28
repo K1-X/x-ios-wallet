66db0c495e6e221ecb26f339aec193b47d8c5a0d
abigailshelf <abigailshelf@hotmail.com>
2021-06-28 09:00:01 +0800

    scrollView

--- SuperWallet/Mine/ViewControllers/KeyStoreCodeController.swift
+++ SuperWallet/Mine/ViewControllers/KeyStoreCodeController.swift
@@ -5,5 +5,11 @@ import CoreImage
 
 class KeyStoreCodeController: UIViewController {
 
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var scanTitle: UILabel!
+    @IBOutlet weak var scanNotice: UILabel!
+    @IBOutlet weak var safetyTitle: UILabel!
+    @IBOutlet weak var safetyNotice: UILabel!
+    @IBOutlet weak var codeImageView: UIImageView!
     
 }
