a8f13085c943c8eff5f5254a79c00faba5d6f274
jacobtuto <jacobtuto@hotmail.com>
2021-06-29 10:10:01 +0800

    footerTitle

--- SuperWallet/Wallet/Types/ImportSelectionType.swift
+++ SuperWallet/Wallet/Types/ImportSelectionType.swift
@@ -34,4 +34,17 @@ enum ImportSelectionType {
             self = .keystore
         }
     }
+
+    var footerTitle: String {
+        switch self {
+        case .keystore:
+            return NSLocalizedString("import.keystore.footer", value: "Several lines of text beginning with “{...}” plus the password you used to encrypt it", comment: "")
+        case .privateKey:
+            return NSLocalizedString("import.privateKey.footer", value: "Typically 64 alphanumeric characters", comment: "")
+        case .mnemonic:
+            return NSLocalizedString("import.mnemonic.footer", value: "Typically 12 (sometimes 24) words separated by single spaces", comment: "")
+        case .address:
+            return NSLocalizedString("import.watch.footer", value: "You can “watch” any public address without divulging your private key. This let’s you view balances and transactions, but not send transactions.", comment: "")
+        }
+    }
 }
