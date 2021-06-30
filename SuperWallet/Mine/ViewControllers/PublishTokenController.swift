edb280bec81fde6db4cecb0b7474881c944aa3d3
abigailshelf <abigailshelf@hotmail.com>
2021-06-30 09:24:51 +0800

    PublishTokenController.swift added

---
+++ SuperWallet/Mine/ViewControllers/PublishTokenController.swift
@@ -0,0 +1,14 @@
+// Copyright DApps Platform Inc. All rights reserved.
+
+import UIKit
+import Eureka
+import SnapKit
+import BigInt
+import TrustCore
+import Result
+import APIKit
+import JSONRPCKit
+
+protocol PublishTokenControllerDelegate: class {
+    func selectWalletTab()
+}
