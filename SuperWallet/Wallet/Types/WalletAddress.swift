c5b9818a50c49437ade3cd970475fd934cf6e575
jacobtuto <jacobtuto@hotmail.com>
2021-07-01 07:20:01 +0800

    coin

--- SuperWallet/Wallet/Types/WalletAddress.swift
+++ SuperWallet/Wallet/Types/WalletAddress.swift
@@ -5,5 +5,12 @@ import RealmSwift
 import TrustCore
 
 final class WalletAddress: Object {
-    
+
+    @objc dynamic var id: String = UUID().uuidString
+    @objc dynamic var addressString: String = ""
+    @objc private dynamic var rawCoin = -1
+    public var coin: Coin {
+        get { return Coin(rawValue: rawCoin) ?? .ethereum }
+        set { rawCoin = newValue.rawValue }
+    }    
 }
