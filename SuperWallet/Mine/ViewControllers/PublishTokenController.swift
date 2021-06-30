1aadfe0dd268c4dbd22dd17747d0792fb579c936
abigailshelf <abigailshelf@hotmail.com>
2021-06-30 09:25:27 +0800

    tableView

--- SuperWallet/Mine/ViewControllers/PublishTokenController.swift
+++ SuperWallet/Mine/ViewControllers/PublishTokenController.swift
@@ -12,3 +12,36 @@ import JSONRPCKit
 protocol PublishTokenControllerDelegate: class {
     func selectWalletTab()
 }
+
+class PublishTokenController: UIViewController {
+
+    lazy var network: NetworkProtocol = {
+        return SuperWalletNetwork(
+            provider: SuperWalletProviderFactory.makeProvider(),
+            wallet: session.account
+        )
+    }()
+    lazy var sendTransactionCoordinator = { () -> SendTransactionCoordinator in
+        let server = RPCServer(rawValue: (chainObject?.pkId)!)
+        return SendTransactionCoordinator(session: self.session, keystore: keystore, confirmType: .signThenSend, server: server!)
+    }()
+
+    var requestCount = 0
+    var pkHash: String = ""
+    var timer: DispatchSourceTimer?
+
+    weak var delegate: PublishTokenControllerDelegate?
+
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView(frame: .zero, style: .grouped)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.separatorStyle = .none
+        tableView.backgroundColor = Colors.white
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.tableFooterView = UIView()
+        tableView.register(UINib(resource: R.nib.textInputCell), forCellReuseIdentifier: R.nib.textInputCell.name)
+        return tableView
+    }()
+
+}
