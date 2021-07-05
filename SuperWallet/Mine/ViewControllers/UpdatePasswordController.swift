a86ed61a182a49f057f099ab2cddd5946bcd008a
abigailshelf <abigailshelf@hotmail.com>
2021-07-06 07:20:01 +0800

    UpdatePasswordController

--- SuperWallet/Mine/ViewControllers/UpdatePasswordController.swift
+++ SuperWallet/Mine/ViewControllers/UpdatePasswordController.swift
@@ -8,3 +8,11 @@ protocol UpdatePasswordControllerDelegate: class {
     func updatePassword(wallet: Wallet, newPassword: String)
 }
 
+class UpdatePasswordController: UIViewController {
+    @IBOutlet weak var currentPasswordTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var submitTextField: UITextField!
+    @IBOutlet weak var noticeLabel: UILabel!
+
+}
+
