b049b952205433e1debd6be668566ea97b82362d
abigailshelf <abigailshelf@hotmail.com>
2021-07-06 09:20:01 +0800

    setUpViews

--- SuperWallet/Mine/ViewControllers/UpdatePasswordController.swift
+++ SuperWallet/Mine/ViewControllers/UpdatePasswordController.swift
@@ -37,5 +37,41 @@ class UpdatePasswordController: UIViewController {
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: R.string.localizable.changePasswordDoneTitle(), style: .done, target: self, action: #selector(changePassword))
         setUpViews()
     }
+
+    func setUpViews() {
+        currentPasswordTextField.backgroundColor = Colors.white
+        currentPasswordTextField.tintColor = Colors.blue
+        currentPasswordTextField.layer.cornerRadius = currentPasswordTextField.frame.size.height/2.0
+        currentPasswordTextField.layer.masksToBounds = true
+        currentPasswordTextField.isSecureTextEntry = true
+        currentPasswordTextField.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 20, height: 40))
+        currentPasswordTextField.leftViewMode = .always
+        currentPasswordTextField.clearButtonMode = .whileEditing
+        currentPasswordTextField.placeholder = R.string.localizable.changePasswordCurrentPassword()
+
+        passwordTextField.backgroundColor = Colors.white
+        passwordTextField.tintColor = Colors.blue
+        passwordTextField.layer.cornerRadius = passwordTextField.frame.size.height/2.0
+        passwordTextField.layer.masksToBounds = true
+        passwordTextField.isSecureTextEntry = true
+        passwordTextField.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 20, height: 40))
+        passwordTextField.leftViewMode = .always
+        passwordTextField.clearButtonMode = .whileEditing
+        passwordTextField.placeholder = R.string.localizable.changePasswordInputPassword()
+
+        submitTextField.backgroundColor = Colors.white
+        submitTextField.tintColor = Colors.blue
+        submitTextField.layer.cornerRadius = passwordTextField.frame.size.height/2.0
+        submitTextField.layer.masksToBounds = true
+        submitTextField.isSecureTextEntry = true
+        submitTextField.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 20, height: 40))
+        submitTextField.leftViewMode = .always
+        submitTextField.clearButtonMode = .whileEditing
+        submitTextField.placeholder = R.string.localizable.changePasswordSubmitPassword()
+
+        noticeLabel.backgroundColor = UIColor.clear
+        noticeLabel.text = R.string.localizable.changePasswordNoticeContent()
+        noticeLabel.textColor = Colors.gray
+    }
 }
 
