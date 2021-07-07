2345da57336f2b085ef958167b70c2db4380a868
abigailshelf <abigailshelf@hotmail.com>
2021-07-08 05:50:01 +0800

    WalletEditControllerDelegate

--- SuperWallet/Mine/ViewControllers/WalletEditController.swift
+++ SuperWallet/Mine/ViewControllers/WalletEditController.swift
@@ -12,3 +12,11 @@ enum AlertType {
     case exportKeyStore
 }
 
+protocol WalletEditControllerDelegate: class {
+    func saveWallet(account: Account, walletName: String)
+    func changePassword(viewModel: WalletAccountViewModel)
+    func exportPrivateKey(account: Account, completion: @escaping (Result<Data, KeystoreError>) -> Void)
+    func exportKeystore(account: Account, password: String, completion: @escaping (Result<String, KeystoreError>) -> Void)
+    func deleteWallet(account: Account, completion: @escaping (Result<Void, KeystoreError>) -> Void)
+}
+
