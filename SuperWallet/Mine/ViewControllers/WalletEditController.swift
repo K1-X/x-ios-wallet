84f2d5b1166651d90e9ade874e34a8c516cce5b3
abigailshelf <abigailshelf@hotmail.com>
2021-07-08 06:30:01 +0800

    footerView

--- SuperWallet/Mine/ViewControllers/WalletEditController.swift
+++ SuperWallet/Mine/ViewControllers/WalletEditController.swift
@@ -20,3 +20,36 @@ protocol WalletEditControllerDelegate: class {
     func deleteWallet(account: Account, completion: @escaping (Result<Void, KeystoreError>) -> Void)
 }
 
+class WalletEditController: UIViewController {
+    weak var delegate: WalletEditControllerDelegate?
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView(frame: .zero, style: .grouped)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.separatorStyle = .none
+        tableView.backgroundColor = .white
+        tableView.backgroundColor = Colors.veryLightGray
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.tableFooterView = UIView()
+        tableView.register(UINib(resource: R.nib.editWalletHeaderCell), forCellReuseIdentifier: R.nib.editWalletHeaderCell.name)
+        tableView.register(UINib(resource: R.nib.textInputCell), forCellReuseIdentifier: R.nib.textInputCell.name)
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "customTableViewCell")
+        return tableView
+    }()
+    lazy var deleteButton: UIButton = {
+        let deleteButton: UIButton = UIButton(type: .custom)
+        deleteButton.setBackgroundColor(Colors.lightGray, forState: .normal)
+        deleteButton.setTitleColor(Colors.white, for: .normal)
+        deleteButton.setTitle(R.string.localizable.walletEditDelete(), for: .normal)
+        deleteButton.addTarget(self, action: #selector(deleteWallet), for: .touchUpInside)
+        return deleteButton
+    }()
+
+    lazy var footerView: UIView = {
+        let footerView: UIView = UIView(frame: CGRect(x: 0, y: 0, width: screenWidth, height: 98))
+        footerView.backgroundColor = Colors.veryLightGray
+        footerView.addSubview(deleteButton)
+        return footerView
+    }()
+
+}
