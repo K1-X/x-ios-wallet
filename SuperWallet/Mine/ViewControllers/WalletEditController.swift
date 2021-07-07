24b1065f080723f43905a7299d27ae1ab9abf776
abigailshelf <abigailshelf@hotmail.com>
2021-07-08 05:20:01 +0800

    WalletEditController.swift added

---
+++ SuperWallet/Mine/ViewControllers/WalletEditController.swift
@@ -0,0 +1,14 @@
+// Copyright DApps Platform Inc. All rights reserved.
+
+import UIKit
+import SnapKit
+import MBProgressHUD
+import TrustKeystore
+import TrustCore
+import Result
+
+enum AlertType {
+    case exportPrivate
+    case exportKeyStore
+}
+
