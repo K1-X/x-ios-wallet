1717b1ea6fd40579c54b56f459ea3af8783aa0cf
jacobtuto <jacobtuto@hotmail.com>
2021-07-09 10:10:01 +0800

    init method

--- SuperWallet/Wallet/Types/WalletValueOperation.swift
+++ SuperWallet/Wallet/Types/WalletValueOperation.swift
@@ -6,5 +6,17 @@ import TrustCore
 import BigInt
 
 final class WalletValueOperation: SuperWalletOperation {
-    
+    private var balanceProvider: WalletBalanceProvider
+    private let keystore: Keystore
+    private let wallet: WalletObject
+
+    init(
+        balanceProvider: WalletBalanceProvider,
+        keystore: Keystore,
+        wallet: WalletObject
+        ) {
+        self.balanceProvider = balanceProvider
+        self.keystore = keystore
+        self.wallet = wallet
+    }    
 }
