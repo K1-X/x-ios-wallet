e4697ba720f2f045b36a3b8bf3d2ccbb53fc03e5
jacobtuto <jacobtuto@hotmail.com>
2021-07-09 09:00:01 +0800

    primaryKey

--- SuperWallet/Wallet/Types/WalletObject.swift
+++ SuperWallet/Wallet/Types/WalletObject.swift
@@ -7,5 +7,21 @@ import TrustCore
 
 final class WalletObject: Object {
 
+    @objc dynamic var id: String = ""
+    @objc dynamic var name: String = ""
+    @objc dynamic var createdAt: Date = Date()
+    @objc dynamic var completedBackup: Bool = false
+    @objc dynamic var mainWallet: Bool = false
+    @objc dynamic var balance: String = ""
+
+    override static func primaryKey() -> String? {
+        return "id"
+    }
+
+    static func from(_ type: WalletType) -> WalletObject {
+        let info = WalletObject()
+        info.id = type.description
+        return info
+    }
     
 }
