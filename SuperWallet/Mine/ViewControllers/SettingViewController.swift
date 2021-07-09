2f30f4ddce5934fb33e8222420dd361f7a116299
abigailshelf <abigailshelf@hotmail.com>
2021-07-10 07:50:01 +0800

    updateVersionRow

--- SuperWallet/Mine/ViewControllers/SettingViewController.swift
+++ SuperWallet/Mine/ViewControllers/SettingViewController.swift
@@ -112,4 +112,28 @@ class SettingViewController: FormViewController {
 
         })
     }
+
+    private func updateVersionRow() -> ButtonRow {
+        return AppFormAppearance.button { row in
+            row.cellStyle = .value1
+        }.cellUpdate { cell, _ in
+            cell.textLabel?.textColor = .black
+            cell.textLabel?.text = R.string.localizable.settingUpdateVersionTitle()
+            cell.accessoryType = .disclosureIndicator
+            cell.selectionStyle = .none
+            cell.detailTextLabel?.text = NSString(format: "V%@", Bundle.main.versionNumber!) as String
+        }.cellSetup({ (cell, _) in
+            cell.height = ({ return 48 })
+            let separatorView = UIView(frame: .zero)
+            separatorView.backgroundColor = Colors.veryLightGray
+            cell.addSubview(separatorView)
+            separatorView.snp.makeConstraints({ (make) in
+                make.bottom.equalToSuperview()
+                make.leading.trailing.equalToSuperview()
+                make.height.equalTo(1)
+            })
+        }).onCellSelection({ (_, _) in
+            self.checkUpdate()
+        })
+    }
 }
