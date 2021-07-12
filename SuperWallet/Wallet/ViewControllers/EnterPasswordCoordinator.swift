413edd4fd6ac49200f7f70577f754787796f83f3
jacobtuto <jacobtuto@hotmail.com>
2021-07-12 09:20:01 +0800

    enterPasswordController

--- SuperWallet/Wallet/ViewControllers/EnterPasswordCoordinator.swift
+++ SuperWallet/Wallet/ViewControllers/EnterPasswordCoordinator.swift
@@ -7,3 +7,19 @@ protocol EnterPasswordCoordinatorDelegate: class {
     func didEnterPassword(password: String, account: Account, in coordinator: EnterPasswordCoordinator)
     func didCancel(in coordinator: EnterPasswordCoordinator)
 }
+
+final class EnterPasswordCoordinator: Coordinator {
+    var coordinators: [Coordinator] = []
+
+    weak var delegate: EnterPasswordCoordinatorDelegate?
+
+    lazy var enterPasswordController: EnterPasswordViewController = {
+        let controller = EnterPasswordViewController(account: account)
+        controller.navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(dismiss))
+        controller.delegate = self
+        return controller
+    }()
+    let navigationController: NavigationController
+    private let account: Account
+
+}
