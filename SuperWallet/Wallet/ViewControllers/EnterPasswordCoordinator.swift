62549cf953760f64b1fca5699124d561685efbf7
jacobtuto <jacobtuto@hotmail.com>
2021-07-12 09:00:01 +0800

    EnterPasswordCoordinatorDelegate

--- SuperWallet/Wallet/ViewControllers/EnterPasswordCoordinator.swift
+++ SuperWallet/Wallet/ViewControllers/EnterPasswordCoordinator.swift
@@ -3,3 +3,7 @@
 import Foundation
 import TrustKeystore
 
+protocol EnterPasswordCoordinatorDelegate: class {
+    func didEnterPassword(password: String, account: Account, in coordinator: EnterPasswordCoordinator)
+    func didCancel(in coordinator: EnterPasswordCoordinator)
+}
