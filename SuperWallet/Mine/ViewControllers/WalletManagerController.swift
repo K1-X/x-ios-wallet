12bbf3ee9b5da172b87ed9b3dc43a09dff493302
abigailshelf <abigailshelf@hotmail.com>
2021-07-13 07:50:01 +0800

    WalletManagerController

--- SuperWallet/Mine/ViewControllers/WalletManagerController.swift
+++ SuperWallet/Mine/ViewControllers/WalletManagerController.swift
@@ -9,3 +9,21 @@ protocol WalletManagerControllerDelegate: class {
     )
 }
 
+class WalletManagerController: UITableViewController {
+
+    let keystore: Keystore
+    lazy var viewModel: WalletsViewModel = {
+        let model = WalletsViewModel(keystore: keystore)
+        model.delegate = self
+        return model
+    }()
+
+    weak var delegate: WalletManagerControllerDelegate?
+
+    init(keystore: Keystore) {
+        self.keystore = keystore
+        super.init(style: .grouped)
+    }
+
+}
+
