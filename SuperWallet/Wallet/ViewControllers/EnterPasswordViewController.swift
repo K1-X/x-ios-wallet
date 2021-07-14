0ebdede61bccadab832a67dd9739502eca05cf84
jacobtuto <jacobtuto@hotmail.com>
2021-07-14 09:00:01 +0800

    passwordRow

--- SuperWallet/Wallet/ViewControllers/EnterPasswordViewController.swift
+++ SuperWallet/Wallet/ViewControllers/EnterPasswordViewController.swift
@@ -8,3 +8,17 @@ protocol EnterPasswordViewControllerDelegate: class {
     func didEnterPassword(password: String, for account: Account, in viewController: EnterPasswordViewController)
 }
 
+final class EnterPasswordViewController: FormViewController {
+
+    struct Values {
+        static var password = "password"
+        static var confirmPassword = "confirmPassword"
+    }
+    weak var delegate: EnterPasswordViewControllerDelegate?
+    private let viewModel = EnterPasswordViewModel()
+
+    var passwordRow: TextFloatLabelRow? {
+        return form.rowBy(tag: Values.password) as? TextFloatLabelRow
+    }
+
+}
