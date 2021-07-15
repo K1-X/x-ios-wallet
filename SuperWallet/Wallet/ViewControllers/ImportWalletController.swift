9e8f4ecf3372ef3282e37cc2e4735c270c48c57c
jacobtuto <jacobtuto@hotmail.com>
2021-07-16 07:20:01 +0800

    viewDidLoad

--- SuperWallet/Wallet/ViewControllers/ImportWalletController.swift
+++ SuperWallet/Wallet/ViewControllers/ImportWalletController.swift
@@ -30,4 +30,30 @@ class ImportWalletController: UIViewController {
         return privateWallet
     }()
 
+
+    weak var delegate: ImportWalletControllerDelegate?
+    let keystore: Keystore
+    let coin: Coin
+
+    init(
+        keystore: Keystore,
+        for coin: Coin
+        ) {
+        self.keystore = keystore
+        self.coin = coin
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = Colors.white
+        navigationItem.title = ""
+        navigationItem.rightBarButtonItem = UIBarButtonItem(image: R.image.qr_code_scan(), style: .done, target: self, action: #selector(openReader))
+        childControllerIndex = 0
+        self.initSubViews()
+    }
 }
