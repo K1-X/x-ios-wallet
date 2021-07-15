0519ea7b63891f6a65972a4c74db548a5755bce3
jacobtuto <jacobtuto@hotmail.com>
2021-07-16 06:30:01 +0800

    class ImportWalletController

--- SuperWallet/Wallet/ViewControllers/ImportWalletController.swift
+++ SuperWallet/Wallet/ViewControllers/ImportWalletController.swift
@@ -5,3 +5,29 @@ import SGPagingView
 protocol ImportWalletControllerDelegate: class {
     func didImportAccount(account: WalletInfo, fields: [WalletInfoField], in viewController: ImportWalletController)
 }
+
+class ImportWalletController: UIViewController {
+    /// 
+    private var pageTitleView: SGPageTitleView?
+    private var pageContentView: SGPageContentScrollView?
+    private var childControllerIndex: NSInteger?
+    /// 
+    private lazy var addCameraButton: UIButton = {
+        let addCameraButton = UIButton(frame: CGRect(x: screenWidth - 40, y: 0, width: 40, height: 40))
+        addCameraButton.setImage(UIImage.init(named: "camera"), for: UIControlState.normal)
+        return addCameraButton
+    }()
+
+    lazy var officialWallet: OfficialWalletController = {
+        let officialWallet = OfficialWalletController()
+        officialWallet.delegate = self
+        return officialWallet
+    }()
+
+    lazy var privateWallet: PrivateWalletController = {
+        let privateWallet = PrivateWalletController()
+        privateWallet.delegate = self
+        return privateWallet
+    }()
+
+}
