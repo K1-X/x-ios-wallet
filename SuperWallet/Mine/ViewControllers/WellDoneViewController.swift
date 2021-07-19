0cd004a1e3d41508b6538e303e29d2a16bfaf869
abigailshelf <abigailshelf@hotmail.com>
2021-07-20 07:50:01 +0800

    WellDoneViewControllerDelegate

--- SuperWallet/Mine/ViewControllers/WellDoneViewController.swift
+++ SuperWallet/Mine/ViewControllers/WellDoneViewController.swift
@@ -5,3 +5,8 @@ enum WellDoneAction {
     case other
 }
 
+
+protocol WellDoneViewControllerDelegate: class {
+    func didPress(action: WellDoneAction, sender: UIView, in viewController: WellDoneViewController)
+}
+
