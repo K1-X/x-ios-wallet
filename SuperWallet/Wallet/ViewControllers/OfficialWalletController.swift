5a7c6d82f0bacd9dcd58bca50f1e4f52ebd43777
jacobtuto <jacobtuto@hotmail.com>
2021-07-21 06:30:01 +0800

    tableView

--- SuperWallet/Wallet/ViewControllers/OfficialWalletController.swift
+++ SuperWallet/Wallet/ViewControllers/OfficialWalletController.swift
@@ -6,3 +6,21 @@ protocol OfficialWalletControllerDelegate: class {
                               viewController: OfficialWalletController
     )
 }
+
+final class OfficialWalletController: UIViewController {
+
+    weak var delegate: OfficialWalletControllerDelegate?
+    var dataArray = ["keystore"]
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView(frame: .zero, style: .grouped)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.separatorStyle = .none
+        tableView.backgroundColor = Colors.veryLightGray
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.tableFooterView = UIView()
+        tableView.register(UINib(resource: R.nib.textInputCell), forCellReuseIdentifier: R.nib.textInputCell.name)
+        return tableView
+    }()
+
+}
