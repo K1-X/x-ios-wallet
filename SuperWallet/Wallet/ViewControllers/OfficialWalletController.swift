3c27158081578aad03feacd3ba87c92edf3edab4
jacobtuto <jacobtuto@hotmail.com>
2021-07-21 07:50:01 +0800

    footerView

--- SuperWallet/Wallet/ViewControllers/OfficialWalletController.swift
+++ SuperWallet/Wallet/ViewControllers/OfficialWalletController.swift
@@ -29,4 +29,24 @@ final class OfficialWalletController: UIViewController {
         return headerView
     }()
 
+
+    lazy var footerView: UIView = {
+        let footerView: UIView = UIView(frame: CGRect(x: 0, y: 0, width: screenWidth, height: 156))
+        footerView.backgroundColor = Colors.veryLightGray
+        let importWalletButton: UIButton = UIButton(type: .custom)
+        importWalletButton.setBackgroundColor(Colors.blue, forState: .normal)
+        importWalletButton.layer.cornerRadius = importWalletButton.frame.size.height/2.0
+        importWalletButton.layer.masksToBounds = true
+        importWalletButton.setTitleColor(Colors.white, for: .normal)
+        importWalletButton.setTitle("", for: .normal)
+        importWalletButton.addTarget(self, action: #selector(importWallet), for: .touchUpInside)
+        footerView.addSubview(importWalletButton)
+        importWalletButton.snp.makeConstraints({ (make) in
+            make.top.equalToSuperview().offset(30)
+            make.leading.equalToSuperview().offset(edgeWidth)
+            make.trailing.equalToSuperview().offset(-edgeWidth)
+            make.height.equalTo(48)
+        })
+        return footerView
+    }()
 }
