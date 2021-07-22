993645718ace9b1eccf9cc3198254e2746b1250c
jacobtuto <jacobtuto@hotmail.com>
2021-07-23 07:50:01 +0800

    PassphraseMode

--- SuperWallet/Wallet/ViewControllers/PassphraseViewController.swift
+++ SuperWallet/Wallet/ViewControllers/PassphraseViewController.swift
@@ -8,3 +8,11 @@ protocol PassphraseViewControllerDelegate: class {
     func didPressVerify(in controller: PassphraseViewController, with account: Wallet, words: [String])
 }
 
+enum PassphraseMode {
+    case showOnly
+    case showAndVerify
+}
+
+final class DarkPassphraseViewController: PassphraseViewController {
+
+}
