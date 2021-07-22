0a32c84b288d3273afef5837b7b947beb3791e9b
abigailshelf <abigailshelf@hotmail.com>
2021-07-22 09:00:01 +0800

    AddCustomNetworkViewModel

---
+++ SuperWallet/Mine/ViewModels/AddCustomNetworkViewModel.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+struct AddCustomNetworkViewModel {
+
+    var title: String {
+        return NSLocalizedString("settings.customNetwork.navigation.title", value: "Custom Network", comment: "")
+    }
+}
