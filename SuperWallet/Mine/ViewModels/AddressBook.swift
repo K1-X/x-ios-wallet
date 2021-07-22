0f86aa44bc52b86ecf140bbce9b3bb5de25ba73c
abigailshelf <abigailshelf@hotmail.com>
2021-07-22 10:10:01 +0800

    init method

--- SuperWallet/Mine/ViewModels/AddressBook.swift
+++ SuperWallet/Mine/ViewModels/AddressBook.swift
@@ -4,5 +4,22 @@ import RealmSwift
 
 class AddressBook: Object {
 
+    @objc dynamic var headImageURL: String = ""
+    @objc dynamic var addressName: String = ""
+    @objc dynamic var address: String = ""
+    @objc dynamic var remark: String = ""
+
+    convenience init(
+        headImageURL: String = "",
+        addressName: String = "",
+        address: String = "",
+        remark: String = ""
+        ) {
+        self.init()
+        self.headImageURL = headImageURL
+        self.addressName = addressName
+        self.address = address
+        self.remark = remark
+    }
     
 }
