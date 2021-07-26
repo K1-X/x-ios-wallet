286dfd6dc3f3fb8172ce8ee9c88a05c7880e0641
abigailshelf <abigailshelf@hotmail.com>
2021-07-26 09:20:01 +0800

    searchEngineTitle

--- SuperWallet/Mine/ViewModels/BrowserConfigurationViewModel.swift
+++ SuperWallet/Mine/ViewModels/BrowserConfigurationViewModel.swift
@@ -4,5 +4,12 @@ import Foundation
 
 struct BrowserConfigurationViewModel {
 
+    var title: String {
+        return NSLocalizedString("settings.browser.title", value: "DApp Browser", comment: "")
+    }
+
+    var searchEngineTitle: String {
+        return NSLocalizedString("settings.browser.searchEngine.title", value: "Search Engine", comment: "")
+    }
     
 }
