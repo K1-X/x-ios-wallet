09d45469d9d6df2031a491cc018617ff9dffbcbe
jacobtuto <jacobtuto@hotmail.com>
2021-07-27 05:50:01 +0800

    tableView

--- SuperWallet/Wallet/ViewControllers/PrivateWalletController.swift
+++ SuperWallet/Wallet/ViewControllers/PrivateWalletController.swift
@@ -8,3 +8,20 @@ protocol PrivateWalletControllerDelegate: class {
     )
 }
 
+final class PrivateWalletController: UIViewController {
+
+    weak var delegate: PrivateWalletControllerDelegate?
+    var dataArray = ["", "", "（）"]
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView(frame: .zero, style: .grouped)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.separatorStyle = .none
+        tableView.backgroundColor = Colors.veryLightGray
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.tableFooterView = UIView()
+        tableView.register(UINib(resource: R.nib.textInputCell), forCellReuseIdentifier: R.nib.textInputCell.name)
+        return tableView
+    }()
+
+}
