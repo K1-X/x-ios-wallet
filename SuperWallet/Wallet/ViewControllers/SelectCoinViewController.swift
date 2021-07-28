d669a206585314dd68bb58ff5f183a6f05d1cc8a
jacobtuto <jacobtuto@hotmail.com>
2021-07-29 07:20:01 +0800

    tableView

--- SuperWallet/Wallet/ViewControllers/SelectCoinViewController.swift
+++ SuperWallet/Wallet/ViewControllers/SelectCoinViewController.swift
@@ -8,3 +8,24 @@ protocol SelectCoinViewControllerDelegate: class {
     func didSelect(coin: Coin, in controller: SelectCoinViewController)
 }
 
+class SelectCoinViewController: UIViewController {
+
+    lazy var viewModel: SelectCoinsViewModel = {
+        let elements = coins.map { CoinViewModel(coin: $0) }
+        return SelectCoinsViewModel(elements: elements)
+    }()
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView(frame: .zero, style: .grouped)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.separatorStyle = .none
+        tableView.backgroundColor = Colors.white
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.tableFooterView = UIView()
+        tableView.rowHeight = UITableViewAutomaticDimension
+        tableView.register(UINib(resource: R.nib.coinViewCell), forCellReuseIdentifier: R.nib.coinViewCell.name)
+        return tableView
+    }()
+    let coins: [Coin]
+
+}
