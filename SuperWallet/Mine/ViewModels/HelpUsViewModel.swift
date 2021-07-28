6c745c412f051274f210b2725d474431e57149f5
abigailshelf <abigailshelf@hotmail.com>
2021-07-28 09:20:01 +0800

    sharingText

--- SuperWallet/Mine/ViewModels/HelpUsViewModel.swift
+++ SuperWallet/Mine/ViewModels/HelpUsViewModel.swift
@@ -7,5 +7,27 @@ struct HelpUsViewModel {
     var title: String {
         return NSLocalizedString("welldone.navigation.title", value: "Thank you!", comment: "")
     }
-    
+
+    private let sharingText: [String] = [
+        NSLocalizedString(
+            "welldone.viewmodel.sharing.text1",
+            value: "Here is the app I use to store my ETH and ERC20 tokens.",
+            comment: ""
+        ),
+        NSLocalizedString(
+            "welldone.viewmodel.sharing.text2",
+            value: "Check out SuperWallet - the wallet that lets me securely store my Ethereum and ERC20 tokens.",
+            comment: ""
+        ),
+        NSLocalizedString(
+            "welldone.viewmodel.sharing.text3",
+            value: "I securely store Ethereum and ERC20 tokens in the SuperWallet wallet",
+            comment: ""
+        ),
+        NSLocalizedString(
+            "welldone.viewmodel.sharing.text4",
+            value: "I secure my Ethereum and ERC20 tokens in the SuperWallet wallet.",
+            comment: ""
+        )
+    ]    
 }
