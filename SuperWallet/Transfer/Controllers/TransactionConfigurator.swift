2de90f5b9aa153f47237c2440b36a67b10e10b5f
abigailshelf <abigailshelf@hotmail.com>
2021-08-02 05:50:01 +0800

    PreviewTransaction

--- SuperWallet/Transfer/Controllers/TransactionConfigurator.swift
+++ SuperWallet/Transfer/Controllers/TransactionConfigurator.swift
@@ -6,3 +6,14 @@ import TrustKeystore
 import JSONRPCKit
 import APIKit
 
+public struct PreviewTransaction {
+    let value: BigInt
+    let account: Account
+    let address: EthereumAddress?
+    let contract: EthereumAddress?
+    let nonce: BigInt
+    let data: Data
+    let gasPrice: BigInt
+    let gasLimit: BigInt
+    let transfer: Transfer
+}
