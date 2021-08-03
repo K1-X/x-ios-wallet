ae946fab797b46c8fc9ba7a6ecaf8370f07c139b
jacobtuto <jacobtuto@hotmail.com>
2021-08-03 11:40:01 +0800

    Sequence

--- SuperWallet/Wallet/ViewControllers/VerifyPassphraseViewController.swift
+++ SuperWallet/Wallet/ViewControllers/VerifyPassphraseViewController.swift
@@ -227,3 +227,12 @@ extension MutableCollection {
         }
     }
 }
+
+extension Sequence {
+    /// Returns an array with the contents of this sequence, shuffled.
+    func shuffled() -> [Element] {
+        var result = Array(self)
+        result.shuffle()
+        return result
+    }
+}
