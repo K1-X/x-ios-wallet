5ddc917d28354a23f7392a37dc06b6ba38fe43ff
abigailshelf <abigailshelf@hotmail.com>
2021-08-04 09:00:01 +0800

    addressBookTitle

--- SuperWallet/Mine/ViewModels/MineViewModel.swift
+++ SuperWallet/Mine/ViewModels/MineViewModel.swift
@@ -24,4 +24,8 @@ struct MineViewModel {
     var publishTokenTitle: String {
         return NSLocalizedString("mine.publishToken.button.title", value: "publishToken", comment: "")
     }
+
+    var addressBookTitle: String {
+        return NSLocalizedString("mine.addressBook.button.title", value: "addressBook", comment: "")
+    }
 }
