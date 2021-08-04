9327487efed1105e22553ab272d27653b2e4ea32
jacobtuto <jacobtuto@hotmail.com>
2021-08-05 06:30:01 +0800

    final class WalletCreateController

--- SuperWallet/Wallet/ViewControllers/WalletCreateController.swift
+++ SuperWallet/Wallet/ViewControllers/WalletCreateController.swift
@@ -8,3 +8,26 @@ protocol WalletCreateControllerDelegate: class {
     func didPressImportWallet(in viewController: WalletCreateController)
 }
 
+final class WalletCreateController: UIViewController {
+
+    let keystore: Keystore
+    weak var delegate: WalletCreateControllerDelegate?
+    var dataArray = ["", "", ""]
+
+    var walletName: String?
+    var password: String?
+    var subPassword: String?
+
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView(frame: .zero, style: .grouped)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.separatorStyle = .none
+        tableView.backgroundColor = Colors.white
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.tableFooterView = UIView()
+        tableView.register(UINib(resource: R.nib.textInputCell), forCellReuseIdentifier: R.nib.textInputCell.name)
+        return tableView
+    }()
+
+}
