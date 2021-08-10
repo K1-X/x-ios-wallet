b24d1ee4521bae38544cbfaf9ad12dbd1999ee7a
abigailshelf <abigailshelf@hotmail.com>
2021-08-10 09:00:01 +0800

    checksum

--- SuperWallet/Mine/ViewModels/VersionCheck.swift
+++ SuperWallet/Mine/ViewModels/VersionCheck.swift
@@ -3,5 +3,13 @@
 import UIKit
 
 class VersionCheck: NSObject, Decodable {
-    
+
+     @objc dynamic var appVersion: Int = 0
+    @objc dynamic var isForceUpgrade: String = ""
+    @objc dynamic var downloadUrl: String = ""
+    @objc dynamic var desc: String = ""
+    @objc dynamic var isNew: String = ""
+    @objc dynamic var packageSize: Int = 0
+    @objc dynamic var displayVer: String = ""
+    @objc dynamic var checksum: String = ""    
 }
