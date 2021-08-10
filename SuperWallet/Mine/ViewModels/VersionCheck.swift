5d5527aad84fc8dc49e33949dc124ad61a49bd4d
abigailshelf <abigailshelf@hotmail.com>
2021-08-10 09:20:01 +0800

    convenience

--- SuperWallet/Mine/ViewModels/VersionCheck.swift
+++ SuperWallet/Mine/ViewModels/VersionCheck.swift
@@ -12,4 +12,26 @@ class VersionCheck: NSObject, Decodable {
     @objc dynamic var packageSize: Int = 0
     @objc dynamic var displayVer: String = ""
     @objc dynamic var checksum: String = ""    
+ 
+     convenience init(
+        appVersion: Int,
+        isForceUpgrade: String,
+        downloadUrl: String,
+        desc: String,
+        isNew: String,
+        packageSize: Int,
+        displayVer: String,
+        checksum: String
+        ) {
+        self.init()
+        self.appVersion = appVersion
+        self.isForceUpgrade = isForceUpgrade
+        self.downloadUrl = downloadUrl
+        self.desc = desc
+        self.isNew = isNew
+        self.packageSize = packageSize
+        self.displayVer = displayVer
+        self.checksum = checksum
+    }
+
 }
