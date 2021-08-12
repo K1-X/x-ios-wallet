6c4c201b4fbf36bf4a9421a5fb6e23919d051a47
abigailshelf <abigailshelf@hotmail.com>
2021-08-12 09:00:01 +0800

    remarkLabel

--- SuperWallet/Mine/Views/AddressBookCell.swift
+++ SuperWallet/Mine/Views/AddressBookCell.swift
@@ -4,5 +4,10 @@ import UIKit
 
 class AddressBookCell: UITableViewCell {
 
+    @IBOutlet weak var bgView: UIView!
+    @IBOutlet weak var headerImageView: UIImageView!
+    @IBOutlet weak var addressNameLabel: UILabel!
+    @IBOutlet weak var addressLabel: UILabel!
+    @IBOutlet weak var remarkLabel: UILabel!
     
 }
