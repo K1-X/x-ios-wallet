6ad1042f14399fed6e321ff9342afbb180a1b38d
abigailshelf <abigailshelf@hotmail.com>
2021-08-12 10:10:01 +0800

    awakeFromNib

--- SuperWallet/Mine/Views/AddressBookCell.swift
+++ SuperWallet/Mine/Views/AddressBookCell.swift
@@ -16,4 +16,24 @@ class AddressBookCell: UITableViewCell {
         addressLabel.text = addressBook.address
         remarkLabel.text = addressBook.remark
     }
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        selectionStyle = .none
+        self.contentView.backgroundColor = Colors.white
+        bgView.backgroundColor = Colors.white
+        let shadowView: UIView = UIView(frame: bgView.frame)
+        self.contentView.addSubview(shadowView)
+        shadowView.layer.shadowColor = Colors.black.cgColor
+        shadowView.layer.shadowOffset = CGSize(width: 0, height: 0)
+        shadowView.layer.shadowOpacity = 0.8
+        shadowView.layer.shadowRadius = 1.0
+        shadowView.layer.cornerRadius = 4.0
+        shadowView.clipsToBounds = false
+        shadowView.addSubview(bgView)
+
+        bgView.layer.cornerRadius = 4.0
+        bgView.layer.masksToBounds = true
+        addressLabel.lineBreakMode = .byTruncatingMiddle
+    }
 }
