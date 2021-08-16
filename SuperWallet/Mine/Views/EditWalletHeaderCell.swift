430fa4b5c5bc95191770eaf0b9a76d5c081e4f41
abigailshelf <abigailshelf@hotmail.com>
2021-08-16 09:00:01 +0800

    walletNameTextLabel

--- SuperWallet/Mine/Views/EditWalletHeaderCell.swift
+++ SuperWallet/Mine/Views/EditWalletHeaderCell.swift
@@ -4,5 +4,10 @@ import UIKit
 
 class EditWalletHeaderCell: UITableViewCell {
 
+    @IBOutlet weak var bgView: UIView!
+    @IBOutlet weak var walletHeaderView: UIImageView!
+    @IBOutlet weak var amountLabel: UILabel!
+    @IBOutlet weak var walletAddressLabel: UILabel!
+    @IBOutlet weak var walletNameTextLabel: UILabel!
     
 }
