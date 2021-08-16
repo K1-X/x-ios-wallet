d41503e013bc711c60bb2781395016cc0518dd05
abigailshelf <abigailshelf@hotmail.com>
2021-08-16 09:20:01 +0800

    WalletAccountViewModel

--- SuperWallet/Mine/Views/EditWalletHeaderCell.swift
+++ SuperWallet/Mine/Views/EditWalletHeaderCell.swift
@@ -9,5 +9,16 @@ class EditWalletHeaderCell: UITableViewCell {
     @IBOutlet weak var amountLabel: UILabel!
     @IBOutlet weak var walletAddressLabel: UILabel!
     @IBOutlet weak var walletNameTextLabel: UILabel!
-    
+
+
+    var viewModel: WalletAccountViewModel? {
+        didSet {
+            guard let model = viewModel else {
+                return
+            }
+            walletHeaderView.image = model.image
+            walletAddressLabel.text = model.currentWallet?.address.description
+            amountLabel.text = model.balance
+        }
+    }    
 }
