5411992ee9e61a087c38cfec4c6a13f0dcab4c13
abigailshelf <abigailshelf@hotmail.com>
2021-08-18 05:50:01 +0800

    contentView

--- SuperWallet/Mine/Views/ExportPrivateView.swift
+++ SuperWallet/Mine/Views/ExportPrivateView.swift
@@ -6,5 +6,20 @@ import MBProgressHUD
 
 class ExportPrivateView: UIView {
 
+    lazy var backgroundView: UIView = {
+        let backgroundView = UIView()
+        backgroundView.backgroundColor = Colors.black
+        backgroundView.alpha = 0
+        backgroundView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(closeTap)))
+        return backgroundView
+    }()
+
+    lazy var contentView: UIView = {
+        let contentView = UIView()
+        contentView.backgroundColor = Colors.white
+        contentView.layer.cornerRadius = 6
+        contentView.layer.masksToBounds = true
+        return contentView
+    }()
     
 }
