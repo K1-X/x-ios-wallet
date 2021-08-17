f33c72d5d01ab5143fc44250dd66a7b3417e24a6
jacobtuto <jacobtuto@hotmail.com>
2021-08-17 09:00:01 +0800

    keystorePlaceholder

--- SuperWallet/Wallet/ViewModels/ImportWalletViewModel.swift
+++ SuperWallet/Wallet/ViewModels/ImportWalletViewModel.swift
@@ -5,5 +5,17 @@ import TrustCore
 
 struct ImportWalletViewModel {
 
+    private let coin: CoinViewModel
+    init(
+        coin: Coin
+    ) {
+        self.coin = CoinViewModel(coin: coin)
+    }
+    var title: String {
+        return R.string.localizable.importWalletImportButtonTitle() + " " + coin.name
+    }
+    var keystorePlaceholder: String {
+        return R.string.localizable.keystoreJSON()
+    }
     
 }
