5abcf316169255872eb78202d2e74ee91ad848e1
jacobtuto <jacobtuto@hotmail.com>
2021-08-19 07:50:01 +0800

    WalletAccountViewModel.swift

---
+++ SuperWallet/Wallet/ViewModels/WalletAccountViewModel.swift
@@ -0,0 +1,11 @@
+// Copyright DApps Platform Inc. All rights reserved.
+
+import Foundation
+import UIKit
+import TrustKeystore
+import TrustCore
+import BigInt
+
+struct WalletAccountViewModel {
+    
+}
