5032b1dd81eeb63f69fc6a02b87294efa889cab8
jacobtuto <jacobtuto@hotmail.com>
2021-08-19 09:00:01 +0800

    isBalanceHidden

--- SuperWallet/Wallet/ViewModels/WalletAccountViewModel.swift
+++ SuperWallet/Wallet/ViewModels/WalletAccountViewModel.swift
@@ -7,5 +7,24 @@ import TrustCore
 import BigInt
 
 struct WalletAccountViewModel {
-    
+
+    let keystore: Keystore
+    let wallet: WalletInfo
+    let account: Account
+    let currentWallet: WalletInfo?
+    private let shortFormatter = EtherNumberFormatter.short
+
+    var title: String {
+        if wallet.multiWallet {
+            return wallet.info.name
+        }
+        if !wallet.info.name.isEmpty {
+            return  wallet.info.name + " (" + wallet.coin!.server.symbol + ")"
+        }
+        return WalletInfo.emptyName
+    }
+
+    var isBalanceHidden: Bool {
+        return wallet.multiWallet
+    }    
 }
