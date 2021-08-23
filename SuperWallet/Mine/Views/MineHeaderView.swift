7ea82f1a68f49cc33baa38b215a1f30d467e7130
abigailshelf <abigailshelf@hotmail.com>
2021-08-24 07:20:01 +0800

    bgImageView

--- SuperWallet/Mine/Views/MineHeaderView.swift
+++ SuperWallet/Mine/Views/MineHeaderView.swift
@@ -2,6 +2,12 @@ import UIKit
 
 class MineHeaderView: UIView {
 
-    
+    lazy var bgImageView: UIImageView = {
+        let imageView = UIImageView(frame: .zero)
+        imageView.isUserInteractionEnabled = true
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.image = R.image.personal_center_bg()
+        return imageView
+    }()    
 
 }
