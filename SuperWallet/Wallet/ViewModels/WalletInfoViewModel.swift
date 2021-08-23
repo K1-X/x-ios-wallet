401627329c7ae630d3757f890043a4036c5ecc2e
jacobtuto <jacobtuto@hotmail.com>
2021-08-23 09:00:01 +0800

    FormSection

--- SuperWallet/Wallet/ViewModels/WalletInfoViewModel.swift
+++ SuperWallet/Wallet/ViewModels/WalletInfoViewModel.swift
@@ -2,3 +2,15 @@
 
 import Foundation
 import TrustKeystore
+
+struct FormSection {
+    let footer: String?
+    let header: String?
+    let rows: [WalletInfoType]
+
+    init(footer: String? = .none, header: String? = .none, rows: [WalletInfoType]) {
+        self.footer = footer
+        self.header = header
+        self.rows = rows
+    }
+}
