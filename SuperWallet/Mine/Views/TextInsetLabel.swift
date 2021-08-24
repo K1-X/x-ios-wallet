3de8426225579784fb332de5b8243d4dad850197
abigailshelf <abigailshelf@hotmail.com>
2021-08-24 11:40:01 +0800

    drawText

--- SuperWallet/Mine/Views/TextInsetLabel.swift
+++ SuperWallet/Mine/Views/TextInsetLabel.swift
@@ -4,5 +4,18 @@ class TextInsetLabel: UILabel {
 
     var edgeInsets: UIEdgeInsets?
 
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        edgeInsets = .zero
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        edgeInsets = .zero
+        super.init(coder: aDecoder)
+    }
+
+    override func drawText(in rect: CGRect) {
+        super.drawText(in: UIEdgeInsetsInsetRect(rect, edgeInsets!))
+    }
     
 }
