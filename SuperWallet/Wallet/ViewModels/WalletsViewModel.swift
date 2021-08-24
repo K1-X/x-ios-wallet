3d09a5d1b0ada621064d1d88f059f47dd49b00cb
jacobtuto <jacobtuto@hotmail.com>
2021-08-25 07:50:01 +0800

    class WalletsViewModel

--- SuperWallet/Wallet/ViewModels/WalletsViewModel.swift
+++ SuperWallet/Wallet/ViewModels/WalletsViewModel.swift
@@ -7,3 +7,21 @@ import TrustCore
 protocol WalletsViewModelProtocol: class {
     func update()
 }
+
+class WalletsViewModel {
+    private let keystore: Keystore
+    private let networks: [WalletInfo] = []
+    private let importedWallet: [WalletInfo] = []
+
+    var sections: [WalletAccountViewModel] = []
+    private let operationQueue: OperationQueue = OperationQueue()
+    weak var delegate: WalletsViewModelProtocol?
+
+    init(
+        keystore: Keystore
+    ) {
+        self.keystore = keystore
+    }
+
+}
+
