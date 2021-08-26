f569849db9383fc13c7b071c6ac2823597ce9dc6
abigailshelf <abigailshelf@hotmail.com>
2021-08-26 10:10:01 +0800

    awakeFromNib

--- SuperWallet/Mine/Views/WalletManagerCell.swift
+++ SuperWallet/Mine/Views/WalletManagerCell.swift
@@ -23,4 +23,27 @@ class WalletManagerCell: UITableViewCell {
             amountLabel.text = model.balance
         }
     }
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        selectionStyle = .none
+        self.contentView.backgroundColor = Colors.white
+        underLineView.backgroundColor = Colors.veryLightGray
+        bgView.backgroundColor = Colors.white
+        walletAddressLabel.lineBreakMode = .byTruncatingMiddle
+
+        let shadowView: UIView = UIView(frame: bgView.frame)
+        self.contentView.addSubview(shadowView)
+        shadowView.layer.shadowColor = Colors.black.cgColor
+        shadowView.layer.shadowOffset = CGSize(width: 0, height: 0)
+        shadowView.layer.shadowOpacity = 0.8
+        shadowView.layer.shadowRadius = 1.0
+        shadowView.layer.cornerRadius = 4.0
+        shadowView.clipsToBounds = false
+        shadowView.addSubview(bgView)
+        contentView.sendSubview(toBack: shadowView)
+
+        bgView.layer.cornerRadius = 4.0
+        bgView.layer.masksToBounds = true
+    }
 }
