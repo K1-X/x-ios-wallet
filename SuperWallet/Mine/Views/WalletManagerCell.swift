2fb1a65d8dcbf75f0783e3ef1ca2efcb68773e46
abigailshelf <abigailshelf@hotmail.com>
2021-08-26 09:00:01 +0800

    amountLabel

--- SuperWallet/Mine/Views/WalletManagerCell.swift
+++ SuperWallet/Mine/Views/WalletManagerCell.swift
@@ -4,6 +4,12 @@ import UIKit
 
 class WalletManagerCell: UITableViewCell {
 
-    
+    @IBOutlet weak var bgView: UIImageView!
+    @IBOutlet weak var walletHeaderView: UIImageView!
+    @IBOutlet weak var walletNameLabel: UILabel!
+    @IBOutlet weak var walletAddressLabel: UILabel!
+    @IBOutlet weak var underLineView: UIView!
+    @IBOutlet weak var walletBlockLabel: UILabel!
+    @IBOutlet weak var amountLabel: UILabel!    
 
 }
