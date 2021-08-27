c1fd3c0292d7cbba8a13a3177966dd8b7f08d30d
jacobtuto <jacobtuto@hotmail.com>
2021-08-27 09:00:01 +0800

    noticeLabel

--- SuperWallet/Wallet/Views/ImportWalletHeaderView.swift
+++ SuperWallet/Wallet/Views/ImportWalletHeaderView.swift
@@ -5,5 +5,13 @@ import SnapKit
 
 class ImportWalletHeaderView: UIView {
 
+    lazy var noticeLabel: UILabel = {
+        let noticeLabel = UILabel()
+        noticeLabel.numberOfLines = 0
+        noticeLabel.textColor = Colors.gray
+        noticeLabel.font = UIFont.systemFont(ofSize: 15)
+        noticeLabel.text = "KeyStore。KeyStore，"
+        return noticeLabel
+    }()
     
 }
