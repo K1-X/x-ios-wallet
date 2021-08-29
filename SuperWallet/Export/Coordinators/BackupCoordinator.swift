638652f7dc9b3319eb58ed847ffa9eb586613769
abigailshelf <abigailshelf@hotmail.com>
2021-08-30 06:30:01 +0800

    final class BackupCoordinator

--- SuperWallet/Export/Coordinators/BackupCoordinator.swift
+++ SuperWallet/Export/Coordinators/BackupCoordinator.swift
@@ -9,3 +9,24 @@ protocol BackupCoordinatorDelegate: class {
     func didFinish(wallet: Wallet, in coordinator: BackupCoordinator)
 }
 
+final class BackupCoordinator: Coordinator {
+
+    let navigationController: NavigationController
+    weak var delegate: BackupCoordinatorDelegate?
+    let keystore: Keystore
+    let account: Account
+    var coordinators: [Coordinator] = []
+
+    init(
+        navigationController: NavigationController,
+        keystore: Keystore,
+        account: Account
+    ) {
+        self.navigationController = navigationController
+        self.navigationController.modalPresentationStyle = .formSheet
+        self.keystore = keystore
+        self.account = account
+    }
+
+}
+
