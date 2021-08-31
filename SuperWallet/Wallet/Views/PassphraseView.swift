e2830a026c52a1e4b5be1fa23b46d4e934815cae
jacobtuto <jacobtuto@hotmail.com>
2021-08-31 09:20:01 +0800

    activate

--- SuperWallet/Wallet/Views/PassphraseView.swift
+++ SuperWallet/Wallet/Views/PassphraseView.swift
@@ -3,3 +3,51 @@
 import Foundation
 import UIKit
 
+final class PassphraseView: UIView {
+
+    lazy var layout: UICollectionViewLayout = {
+        let layout = UICollectionViewFlowLayout()
+        layout.minimumLineSpacing = 8
+        layout.minimumInteritemSpacing = 8
+        layout.estimatedItemSize = UICollectionViewFlowLayoutAutomaticSize
+        layout.sectionInset = UIEdgeInsets(top: 8, left: 0, bottom: 8, right: 0)
+        return layout
+    }()
+    lazy var collectionView: DynamicCollectionView = {
+        let collectionView = DynamicCollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.isScrollEnabled = false
+        return collectionView
+    }()
+
+    var words: [String] = [] {
+        didSet {
+            collectionView.reloadData()
+        }
+    }
+    var isEditable: Bool = false
+    var didDeleteItem: ((String) -> Void)?
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+        addSubview(collectionView)
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.backgroundColor = .white
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        collectionView.register(R.nib.wordCollectionViewCell)
+
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: topAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: bottomAnchor)
+        ])
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+
