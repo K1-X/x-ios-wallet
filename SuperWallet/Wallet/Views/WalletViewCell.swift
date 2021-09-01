e507fad66093066208220be731b1de60e0972ca2
jacobtuto <jacobtuto@hotmail.com>
2021-09-02 07:20:01 +0800

    invalid

--- SuperWallet/Wallet/Views/WalletViewCell.swift
+++ SuperWallet/Wallet/Views/WalletViewCell.swift
@@ -3,3 +3,12 @@
 import TrustCore
 import UIKit
 
+@IBOutlet weak var infoButton: UIButton!
+    @IBOutlet weak var glassesImageView: UIImageView!
+    @IBOutlet weak var walletTypeImageView: UIImageView!
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var subtitle: UILabel!
+    @IBOutlet weak var identiconImageView: TokenImageView!
+    @IBOutlet weak var selectedImageView: UIImageView!
+    @IBOutlet weak var balance: UILabel!
+
