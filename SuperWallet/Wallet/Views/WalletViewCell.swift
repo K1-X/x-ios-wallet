a1aa11f08116c6b1cdc80047df67a810d411ab74
jacobtuto <jacobtuto@hotmail.com>
2021-09-02 07:50:01 +0800

    WalletViewCellDelegate

--- SuperWallet/Wallet/Views/WalletViewCell.swift
+++ SuperWallet/Wallet/Views/WalletViewCell.swift
@@ -3,12 +3,9 @@
 import TrustCore
 import UIKit
 
-@IBOutlet weak var infoButton: UIButton!
-    @IBOutlet weak var glassesImageView: UIImageView!
-    @IBOutlet weak var walletTypeImageView: UIImageView!
-    @IBOutlet weak var title: UILabel!
-    @IBOutlet weak var subtitle: UILabel!
-    @IBOutlet weak var identiconImageView: TokenImageView!
-    @IBOutlet weak var selectedImageView: UIImageView!
-    @IBOutlet weak var balance: UILabel!
+protocol WalletViewCellDelegate: class {
+    func didPress(viewModel: WalletAccountViewModel, in cell: WalletViewCell)
+}
+
+
 
