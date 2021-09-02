f015911267438d827b44f522af00f13ba0002642
jacobtuto <jacobtuto@hotmail.com>
2021-09-02 09:00:01 +0800

    class WalletViewCell

--- SuperWallet/Wallet/Views/WalletViewCell.swift
+++ SuperWallet/Wallet/Views/WalletViewCell.swift
@@ -7,5 +7,17 @@ protocol WalletViewCellDelegate: class {
     func didPress(viewModel: WalletAccountViewModel, in cell: WalletViewCell)
 }
 
+final class WalletViewCell: UITableViewCell {
+
+    @IBOutlet weak var infoButton: UIButton!
+    @IBOutlet weak var glassesImageView: UIImageView!
+    @IBOutlet weak var walletTypeImageView: UIImageView!
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var subtitle: UILabel!
+    @IBOutlet weak var identiconImageView: TokenImageView!
+    @IBOutlet weak var selectedImageView: UIImageView!
+    @IBOutlet weak var balance: UILabel!
+
+}
 
 
