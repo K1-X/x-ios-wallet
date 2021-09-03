46e6e1004069c29e11534d5fad702062b01e8e9c
abigailshelf <abigailshelf@hotmail.com>
2021-09-03 09:20:01 +0800

    hintLabel

--- SuperWallet/Export/ViewControllers/ExportPrivateKeyViewController.swift
+++ SuperWallet/Export/ViewControllers/ExportPrivateKeyViewController.swift
@@ -7,5 +7,24 @@ import MBProgressHUD
 
 final class ExportPrivateKeyViewConroller: UIViewController {
 
+    private struct Layout {
+        static var widthAndHeight: CGFloat = 260
+    }
+
+    lazy var imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+
+    lazy var hintLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.adjustsFontSizeToFitWidth = true
+        label.text = viewModel.headlineText
+        label.textColor = Colors.red
+        label.adjustsFontSizeToFitWidth = true
+        return label
+    }()
     
 }
