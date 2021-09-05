2e844d08d6c05f450ab401d85292997817995ab7
jacobtuto <jacobtuto@hotmail.com>
2021-09-06 07:50:01 +0800

    init method

--- SuperWallet/Export/ViewModels/ExportPhraseViewModel.swift
+++ SuperWallet/Export/ViewModels/ExportPhraseViewModel.swift
@@ -5,5 +5,15 @@ import TrustKeystore
 
 struct ExportPhraseViewModel {
 
+    let keystore: Keystore
+    let account: Wallet
+
+    init(
+        keystore: Keystore,
+        account: Wallet
+    ) {
+        self.keystore = keystore
+        self.account = account
+    }
     
 }
