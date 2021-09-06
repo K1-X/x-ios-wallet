dad52fc5de00ead6db5ba6d9acee53b5536a6979
jacobtuto <jacobtuto@hotmail.com>
2021-09-06 11:40:01 +0800

    warningText

--- SuperWallet/Export/ViewModels/ExportPrivateKeyViewModel.swift
+++ SuperWallet/Export/ViewModels/ExportPrivateKeyViewModel.swift
@@ -16,4 +16,16 @@ struct ExportPrivateKeyViewModel {
     var headlineText: String {
         return NSLocalizedString("export.warning.private.key", value: "Export at your own risk!", comment: "")
     }    
+
+    var privateKeyString: String {
+        return privateKey.hexString
+    }
+
+    var warningText: String {
+        return NSLocalizedString("export.warningTwo.private.key", value: "Anyone with your Private Key will have FULL access to your wallet!", comment: "")
+    }
+
+    var backgroundColor: UIColor {
+        return .white
+    }
 }
