62087f9490a9ee01fb4deff5168dafc2d4ccd3f8
jacobtuto <jacobtuto@hotmail.com>
2021-09-07 07:50:01 +0800

    removeCoordinator

--- SuperWallet/Foundation/Coordinator.swift
+++ SuperWallet/Foundation/Coordinator.swift
@@ -5,3 +5,14 @@ import Foundation
 protocol Coordinator: class {
     var coordinators: [Coordinator] { get set }
 }
+
+extension Coordinator {
+    func addCoordinator(_ coordinator: Coordinator) {
+        coordinators.append(coordinator)
+    }
+
+    func removeCoordinator(_ coordinator: Coordinator) {
+        coordinators = coordinators.filter { $0 !== coordinator }
+    }
+
+}
