3d08ab748a4fb0e7133eedc7bc9ce0d2912006f0
abigailshelf <abigailshelf@hotmail.com>
2021-09-09 07:50:01 +0800

    awakeFromNib

--- SuperWallet/Wallet/WordCollectionViewCell.swift
+++ SuperWallet/Wallet/WordCollectionViewCell.swift
@@ -6,5 +6,13 @@ final class WordCollectionViewCell: UICollectionViewCell {
 
     @IBOutlet weak var wordLabel: UILabel!
 
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+
+        layer.borderColor = UIColor(hex: "C6C6C6").cgColor
+        layer.borderWidth = 0.5
+        layer.cornerRadius = 4
+    }
     
 }
