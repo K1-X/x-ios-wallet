1fe84ecbaa7fb921ce021c5b1b680d80737c80b7
abigailshelf <abigailshelf@hotmail.com>
2021-09-09 07:20:01 +0800

    WordCollectionViewCell

---
+++ SuperWallet/Wallet/WordCollectionViewCell.swift
@@ -0,0 +1,10 @@
+// Copyright DApps Platform Inc. All rights reserved.
+
+import UIKit
+
+final class WordCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var wordLabel: UILabel!
+
+    
+}
