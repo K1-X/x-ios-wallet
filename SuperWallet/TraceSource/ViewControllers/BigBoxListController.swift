a602272b09a58912992d87a399f607dd13279482
jacobtuto <jacobtuto@hotmail.com>
2021-09-09 09:00:01 +0800

    viewDidLoad

--- SuperWallet/TraceSource/ViewControllers/BigBoxListController.swift
+++ SuperWallet/TraceSource/ViewControllers/BigBoxListController.swift
@@ -31,5 +31,19 @@ init(
     self.session = session
     super.init(nibName: nil, bundle: nil)
 }
+
+    required init?(coder aDecoder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+}
+override func viewDidLoad() {
+    super.viewDidLoad()
+    title = ""
+    view.addSubview(tableView)
+    tableView.snp.makeConstraints { (make) in
+        make.leading.trailing.bottom.equalToSuperview()
+        make.top.equalToSuperview().offset(currentNaviHeight)
+    }
+    self.requestData()
+}
   
 }
