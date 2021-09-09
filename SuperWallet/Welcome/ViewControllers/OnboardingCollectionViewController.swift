d28cfed5b98bc4001d5e591d10bef3d48f7caf2f
abigailshelf <abigailshelf@hotmail.com>
2021-09-09 09:00:01 +0800

    OnboardingCollectionViewController.

---
+++ SuperWallet/Welcome/ViewControllers/OnboardingCollectionViewController.swift
@@ -0,0 +1,10 @@
+// Copyright DApps Platform Inc. All rights reserved.
+
+import UIKit
+
+final class OnboardingCollectionViewController: UICollectionViewController {
+    var pages = [OnboardingPageViewModel]()
+    weak var pageControl: UIPageControl?
+
+    
+}
