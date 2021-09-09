666dd4f12add444f89d393bccc2e0d6cecf77cb2
abigailshelf <abigailshelf@hotmail.com>
2021-09-09 09:20:01 +0800

    viewDidLayoutSubviews

--- SuperWallet/Welcome/ViewControllers/OnboardingCollectionViewController.swift
+++ SuperWallet/Welcome/ViewControllers/OnboardingCollectionViewController.swift
@@ -6,5 +6,16 @@ final class OnboardingCollectionViewController: UICollectionViewController {
     var pages = [OnboardingPageViewModel]()
     weak var pageControl: UIPageControl?
 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        collectionView?.register(OnboardingPage.self, forCellWithReuseIdentifier: OnboardingPage.identifier)
+    }
+
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        let flowLayout = collectionViewLayout as! UICollectionViewFlowLayout
+        flowLayout.itemSize = view.bounds.size
+    }
     
 }
