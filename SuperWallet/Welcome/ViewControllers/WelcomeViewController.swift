f981eb41abbaf2de6cc0f2bbb62fd84412269757
abigailshelf <abigailshelf@hotmail.com>
2021-09-13 06:30:01 +0800

    WelcomeViewController.swift

---
+++ SuperWallet/Welcome/ViewControllers/WelcomeViewController.swift
@@ -0,0 +1,8 @@
+import UIKit
+import SnapKit
+
+protocol WelcomeViewControllerDelegate: class {
+    func didPressCreateWallet(in viewController: WelcomeViewController)
+    func didPressImportWallet(in viewController: WelcomeViewController)
+}
+
