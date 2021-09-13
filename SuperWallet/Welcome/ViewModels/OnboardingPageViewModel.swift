190cd5745b7fc11e272d6506ff56564095be56d0
jacobtuto <jacobtuto@hotmail.com>
2021-09-14 07:50:01 +0800

    init method

--- SuperWallet/Welcome/ViewModels/OnboardingPageViewModel.swift
+++ SuperWallet/Welcome/ViewModels/OnboardingPageViewModel.swift
@@ -3,5 +3,14 @@
 import UIKit
 
 struct OnboardingPageViewModel {
-    
+
+    var title: String
+    var subtitle: String
+    var image: UIImage
+
+    init() {
+        title = ""
+        subtitle = ""
+        image = #imageLiteral(resourceName: "onboarding_lock")
+    }    
 }
