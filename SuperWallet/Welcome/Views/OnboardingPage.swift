1567e4b1f2bb7b7b184fa69cd604298cf288bc0f
abigailshelf <abigailshelf@hotmail.com>
2021-09-15 07:50:01 +0800

    OnboardingPage.swift

---
+++ SuperWallet/Welcome/Views/OnboardingPage.swift
@@ -0,0 +1,10 @@
+// Copyright DApps Platform Inc. All rights reserved.
+
+import UIKit
+
+final class OnboardingPage: UICollectionViewCell {
+    static let identifier = "Page"
+    let style = OnboardingPageStyle()
+
+    
+}
