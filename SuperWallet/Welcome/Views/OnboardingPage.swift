1f330a0fb99021560e8fac4babe4796557d08ecb
abigailshelf <abigailshelf@hotmail.com>
2021-09-15 11:40:01 +0800

    activate

--- SuperWallet/Welcome/Views/OnboardingPage.swift
+++ SuperWallet/Welcome/Views/OnboardingPage.swift
@@ -31,4 +31,45 @@ final class OnboardingPage: UICollectionViewCell {
         super.init(coder: coder)
         setup()
     }
+
+    private func setup() {
+        imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFit
+        addSubview(imageView)
+
+        titleLabel = UILabel()
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.textAlignment = .center
+        titleLabel.textColor = style.titleColor
+        titleLabel.font = style.titleFont
+        addSubview(titleLabel)
+
+        subtitleLabel = UILabel()
+        subtitleLabel.translatesAutoresizingMaskIntoConstraints = false
+        subtitleLabel.textAlignment = .center
+        subtitleLabel.textColor = style.subtitleColor
+        subtitleLabel.numberOfLines = 2
+        subtitleLabel.font = style.subtitleFont
+        addSubview(subtitleLabel)
+
+        let stackView = UIStackView(arrangedSubviews: [
+            imageView,
+            titleLabel,
+            subtitleLabel
+        ])
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.alignment = .center
+        stackView.spacing = 15
+        addSubview(stackView)
+
+        NSLayoutConstraint.activate([
+            stackView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            stackView.centerYAnchor.constraint(equalTo: centerYAnchor, constant: -60),
+            stackView.centerXAnchor.constraint(equalTo: centerXAnchor),
+            stackView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            imageView.heightAnchor.constraint(equalToConstant: 240)
+        ])
+    }
 }
