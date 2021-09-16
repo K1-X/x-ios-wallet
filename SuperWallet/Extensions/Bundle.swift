7689c69846dea7094dabd8d03b9afcadbee7e78d
jacobtuto <jacobtuto@hotmail.com>
2021-09-17 07:20:01 +0800

    buildNumber

--- SuperWallet/Extensions/Bundle.swift
+++ SuperWallet/Extensions/Bundle.swift
@@ -3,5 +3,15 @@
 import Foundation
 
 extension Bundle {
-    
+
+    var versionNumber: String? {
+        return infoDictionary?["CFBundleShortVersionString"] as? String
+    }
+    var buildNumber: String? {
+        return infoDictionary?["CFBundleVersion"] as? String
+    }
+
+    var buildNumberInt: Int {
+        return Int(Bundle.main.buildNumber ?? "-1") ?? -1
+    }    
 }
