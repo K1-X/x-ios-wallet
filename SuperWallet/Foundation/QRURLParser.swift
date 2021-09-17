90a6a12c10767922ffec52ec6db42f09cfbf2ddf
jacobtuto <jacobtuto@hotmail.com>
2021-09-18 07:50:01 +0800

    ParserResult

--- SuperWallet/Foundation/QRURLParser.swift
+++ SuperWallet/Foundation/QRURLParser.swift
@@ -1,3 +1,10 @@
 // Copyright DApps Platform Inc. All rights reserved.
 
 import Foundation
+
+struct ParserResult {
+    let protocolName: String
+    let address: String
+    let params: [String: String]
+}
+
