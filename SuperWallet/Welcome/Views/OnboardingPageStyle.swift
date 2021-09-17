965fdde8fffbf00787c84b7bb280ffd2cda18e82
abigailshelf <abigailshelf@hotmail.com>
2021-09-18 07:50:01 +0800

    OnboardingPageStyle

---
+++ SuperWallet/Welcome/Views/OnboardingPageStyle.swift
@@ -0,0 +1,11 @@
+// Copyright DApps Platform Inc. All rights reserved.
+
+import UIKit
+
+struct OnboardingPageStyle {
+    var titleFont: UIFont {
+        return UIFont.systemFont(ofSize: 23, weight: UIFont.Weight.medium)
+    }
+
+    
+}
