b14cca2f7f502b7f671540700da5144da3c895d7
jacobtuto <jacobtuto@hotmail.com>
2021-09-18 07:20:01 +0800

    define:maximumFractionDigits

--- SuperWallet/Foundation/EtherNumberFormatter.swift
+++ SuperWallet/Foundation/EtherNumberFormatter.swift
@@ -5,4 +5,19 @@ import Foundation
 
 final class EtherNumberFormatter {
 
+    /// Formatter that preserves full precision.
+    static let full = EtherNumberFormatter()
+
+    // Formatter that caps the number of decimal digits to 4.
+    static let short: EtherNumberFormatter = {
+        let formatter = EtherNumberFormatter()
+        formatter.maximumFractionDigits = 4
+        return formatter
+    }()
+
+    /// Minimum number of digits after the decimal point.
+    var minimumFractionDigits = 0
+
+    /// Maximum number of digits after the decimal point.
+    var maximumFractionDigits = Int.max
 }
