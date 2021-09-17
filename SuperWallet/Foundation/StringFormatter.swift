9dd0c8ff74882f7e9bfd7ce3e2796e7dc3681818
jacobtuto <jacobtuto@hotmail.com>
2021-09-18 07:50:01 +0800

    currencyFormatter

--- SuperWallet/Foundation/StringFormatter.swift
+++ SuperWallet/Foundation/StringFormatter.swift
@@ -3,5 +3,15 @@
 import UIKit
 
 final class StringFormatter {
-    
+
+    /// currencyFormatter of a `StringFormatter` to represent curent locale.
+    private lazy var currencyFormatter: NumberFormatter = {
+        let formatter = NumberFormatter()
+        formatter.currencySymbol = ""
+        formatter.minimumFractionDigits = 2
+        formatter.maximumFractionDigits = 2
+        formatter.numberStyle = .currencyAccounting
+        formatter.isLenient = true
+        return formatter
+    }()    
 }
