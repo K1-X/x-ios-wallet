b9e48b625ef541d06607a1f39cbd3c445cb090f6
abigailshelf <abigailshelf@hotmail.com>
2021-09-18 09:00:01 +0800

    subtitleFont

--- SuperWallet/Welcome/Views/OnboardingPageStyle.swift
+++ SuperWallet/Welcome/Views/OnboardingPageStyle.swift
@@ -7,5 +7,15 @@ struct OnboardingPageStyle {
         return UIFont.systemFont(ofSize: 23, weight: UIFont.Weight.medium)
     }
 
-    
+    var titleColor: UIColor {
+        return UIColor(hex: "438FCA")
+    }
+
+    var subtitleFont: UIFont {
+        return UIFont.systemFont(ofSize: 15)
+    }
+
+    var subtitleColor: UIColor {
+        return UIColor(hex: "69A5D5")
+    }    
 }
