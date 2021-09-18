60acef516aa5e5ac50405ccca20422785a24097f
abigailshelf <abigailshelf@hotmail.com>
2021-09-18 09:20:01 +0800

    define:var isFinished

--- SuperWallet/Foundation/SuperWalletOperation.swift
+++ SuperWallet/Foundation/SuperWalletOperation.swift
@@ -9,4 +9,16 @@ class SuperWalletOperation: Operation {
         static let isExecuting = "isExecuting"
     }
     
+
+    private var _finished = false
+    private var _isExecuting = false
+
+    override public var isFinished: Bool {
+        get { return _finished }
+        set {
+            willChangeValue(forKey: KVOProperties.isFinished)
+            _finished = newValue
+            didChangeValue(forKey: KVOProperties.isFinished)
+        }
+    }
 }
