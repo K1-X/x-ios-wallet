a14d24c6cac0336b1f45adba2f7d774bb9d6b15d
jacobtuto <jacobtuto@hotmail.com>
2021-10-13 08:37:34 +0800

    preferredLanguageCode

--- SuperWallet/NSLocale.swift
+++ SuperWallet/NSLocale.swift
@@ -4,5 +4,10 @@ import Foundation
 import UIKit
 
 extension NSLocale {
-    
+
+    static var preferredLanguageCode: String? {
+        let langageRegion = NSLocale.preferredLanguages.first ?? ""
+        let languageComponents = NSLocale.components(fromLocaleIdentifier: langageRegion)
+        return languageComponents[NSLocale.Key.languageCode.rawValue]
+    }    
 }
