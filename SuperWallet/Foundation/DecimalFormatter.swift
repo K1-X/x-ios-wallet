22a9cf0cef3475e339f85437ffdaee27028956af
abigailshelf <abigailshelf@hotmail.com>
2021-10-19 05:20:01 +0800

    add:numberFormatter

--- SuperWallet/Foundation/DecimalFormatter.swift
+++ SuperWallet/Foundation/DecimalFormatter.swift
@@ -1,5 +1,15 @@
 import Foundation
 
 final class DecimalFormatter {
-    
+
+    /// Locale of a `DecimalFormatter`.
+    var locale: Locale
+    /// numberFormatter of a `DecimalFormatter` to represent curent locale.
+    private lazy var numberFormatter: NumberFormatter = {
+        let formatter = NumberFormatter()
+        formatter.locale = self.locale
+        formatter.numberStyle = .decimal
+        formatter.isLenient = true
+        return formatter
+    }()    
 }
