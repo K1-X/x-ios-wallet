ca28e233d1e8070ece4cd5c9ab088374ecb4ccb0
jacobtuto <jacobtuto@hotmail.com>
2021-10-19 09:20:01 +0800

    back: UIBarButtonItem

--- SuperWallet/Extensions/UIBarButtonItem.swift
+++ SuperWallet/Extensions/UIBarButtonItem.swift
@@ -4,5 +4,13 @@ import Foundation
 import UIKit
 
 extension UIBarButtonItem {
-    
+
+    static var back: UIBarButtonItem {
+        return UIBarButtonItem(
+            title: R.string.localizable.back(),
+            style: .plain,
+            target: nil,
+            action: nil
+        )
+    }    
 }
