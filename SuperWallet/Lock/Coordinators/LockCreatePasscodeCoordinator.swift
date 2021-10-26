d614a67e32b3d7a633ea4e728cc2ef8460e5d53c
abigailshelf <abigailshelf@hotmail.com>
2021-10-27 07:20:01 +0800

    LockCreatePasscodeCoordinator.swift

---
+++ SuperWallet/Lock/Coordinators/LockCreatePasscodeCoordinator.swift
@@ -0,0 +1,11 @@
+// Copyright DApps Platform Inc. All rights reserved.
+
+import UIKit
+
+protocol LockCreatePasscodeCoordinatorDelegate: class {
+    func didCancel(in coordinator: LockCreatePasscodeCoordinator)
+}
+
+final class LockCreatePasscodeCoordinator: Coordinator {
+    
+}
