da17b3ff6134934889bb0b7e01767d68636ce708
jacobtuto <jacobtuto@hotmail.com>
2021-10-26 09:00:01 +0800

    lazy var lockEnterPasscodeViewController

--- SuperWallet/Lock/Coordinators/AuthenticateUserCoordinator.swift
+++ SuperWallet/Lock/Coordinators/AuthenticateUserCoordinator.swift
@@ -4,5 +4,12 @@ import Foundation
 
 final class AuthenticateUserCoordinator: Coordinator {
 
+    var coordinators: [Coordinator] = []
+    let navigationController: NavigationController
+    private let model: LockEnterPasscodeViewModel
+    private let lock: LockInterface
+    private lazy var lockEnterPasscodeViewController: GestureVerifyViewController = {
+        return GestureVerifyViewController()
+    }()
     
 }
