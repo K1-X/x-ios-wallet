b3b3f93d571a9c1511d468313032c7e96302c1b3
jacobtuto <jacobtuto@hotmail.com>
2021-10-28 09:20:01 +0800

    init methods

--- SuperWallet/Lock/Coordinators/LockEnterPasscodeCoordinator.swift
+++ SuperWallet/Lock/Coordinators/LockEnterPasscodeCoordinator.swift
@@ -3,5 +3,22 @@
 import UIKit
 
 final class LockEnterPasscodeCoordinator: Coordinator {
-    
+
+    var coordinators: [Coordinator] = []
+    let window: UIWindow = UIWindow()
+    private let model: LockEnterPasscodeViewModel
+    private let lock: LockInterface
+    private lazy var lockEnterPasscodeViewController: GestureVerifyViewController = {
+        return GestureVerifyViewController()
+    }()
+    init(model: LockEnterPasscodeViewModel, lock: LockInterface = Lock()) {
+        self.window.windowLevel = UIWindowLevelStatusBar + 1.0
+        self.model = model
+        self.lock = lock
+        lockEnterPasscodeViewController.unlockWithResult = { [weak self] (state, bioUnlock) in
+            if state {
+                self?.stop()
+            }
+        }
+    }    
 }
