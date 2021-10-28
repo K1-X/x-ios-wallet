86ce9b44be6164f11852da46c0685aae5e32a03f
abigailshelf <abigailshelf@hotmail.com>
2021-10-29 07:50:01 +0800

    func:makeConstraints

--- SuperWallet/Lock/ViewControllers/GestureVerifyViewController.swift
+++ SuperWallet/Lock/ViewControllers/GestureVerifyViewController.swift
@@ -22,4 +22,45 @@ class GestureVerifyViewController: UIViewController {
         lock = Lock()
         createView()
     }    
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+
+    func createView() {
+        let msgLabel = SPLockLabel()
+        msgLabel.textAlignment = .center
+        self.msgLabel = msgLabel
+        self.msgLabel?.showNormalMag(msg: gestureTextOldGesture as NSString)
+        view.addSubview(msgLabel)
+
+        //
+        let lockView = SPCircleView()
+        lockView.arrow = true
+        lockView.delegate = self
+        lockView.type = CircleViewType.circleViewTypeVerify
+        self.lockView = lockView
+        view.addSubview(lockView)
+        msgLabel.snp.makeConstraints { (make) in
+            make.top.equalToSuperview().offset(140)
+            make.leading.equalToSuperview().offset(edgeWidth)
+            make.trailing.equalToSuperview().offset(-edgeWidth)
+            make.height.equalTo(20)
+        }
+        lockView.snp.makeConstraints { (make) in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(msgLabel.snp.bottom).offset(20)
+            make.width.equalTo(screenWidth - CircleViewEdgeMargin*2)
+            make.height.equalTo(screenWidth - CircleViewEdgeMargin*2)
+        }
+
+        let infoView = SPCircleInfoView()
+        self.infoView = infoView
+        view.addSubview(infoView)
+        infoView.snp.makeConstraints { (make) in
+            make.centerX.equalToSuperview()
+            make.top.equalToSuperview().offset(navigationHeight + 20)
+            make.size.equalTo(CGSize(width: CircleRadius * 2 * 0.6, height: CircleRadius * 2 * 0.6))
+        }
+    }
 }
